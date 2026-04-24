1cfd4de35f1ec16819a7d675eee68fef89702a49
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-04-24 00:31:33 -0300

    cria classe Biblioteca

---
+++ src/FiapCloundGames.API/Domain/Entities/Biblioteca.cs
@@ -0,0 +1,26 @@
+﻿using FiapCloundGames.API.Domain.Common;
+using FiapCloundGames.API.Domain.Common.Exceptions;
+
+namespace FiapCloundGames.API.Domain.Entities
+{
+    public class Biblioteca : AgreggateRoot
+    {
+        public Guid UsuarioId { get; private set; }
+        public Guid JogoId { get; private set; }
+        public DateTime DataAquisicao { get; private set; }
+        public bool Ativo { get; private set; }
+
+        public Biblioteca(Guid usuarioId, Guid jogoId)
+        {
+            if(usuarioId == Guid.Empty)
+                throw new DomainException("Usuário inválido", nameof(usuarioId));
+            if(jogoId == Guid.Empty)
+                throw new DomainException("Jogo inválido", nameof(jogoId));
+
+            UsuarioId = usuarioId;
+            JogoId = jogoId;
+            DataAquisicao = DateTime.UtcNow;
+            Ativo = true;
+        }
+    }
+}
