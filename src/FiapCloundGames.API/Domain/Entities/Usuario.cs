454babaed23fdf84f07144774906b9adedde9b9c
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-04-25 03:16:56 -0300

    adiciona as validações da entidade usuario e remove a propriedade documento

--- src/FiapCloundGames.API/Domain/Entities/Usuario.cs
+++ src/FiapCloundGames.API/Domain/Entities/Usuario.cs
@@ -9,34 +9,40 @@ namespace FiapCloundGames.API.Domain.Entities
     {
         public Usuario()
         {
-            
+
         }
         public string NomeUsuario { get; private set; }
 
         public string Email { get; private set; }
-       
+
         public string Senha { get; private set; }
-        
+
         public string reSenha { get; private set; }
 
-        public PerfilUsuario Perfil { get; set; }
+        public TipoUsuario Perfil { get; set; }
 
-        public string Documento { get; private set; }
-        public Usuario(string nomeUsuario, string emailUsuario, string senhaUsuario, 
-            string confirmacaoSenhaUsuario, PerfilUsuario perfilUsuario, string documentoUsuario)
+        public Usuario(string nomeUsuario, string emailUsuario, string senhaUsuario,
+            string confirmacaoSenhaUsuario)
         {
             NomeUsuario = nomeUsuario;
-            Email = emailUsuario;   
+            Email = emailUsuario;
             Senha = senhaUsuario;
             reSenha = confirmacaoSenhaUsuario;
-            Documento = documentoUsuario;
-            Perfil = perfilUsuario; 
-            Validar(this);
+            Perfil = TipoUsuario.Jogador;
+            Validar();
         }
 
-        public bool Validar(Usuario usuario)
+        public void Validar()
         {
-            return true;
+            AssertionConcern.AssertArgumentNotEmpty(NomeUsuario, "O nome do usuário é obrigatório.");
+            AssertionConcern.AssertArgumentNotEmpty(Email, "O email do usuário é obrigatório.");
+            AssertionConcern.AssertArgumentNotEmpty(Senha, "A senha do usuário é obrigatória.");
+            AssertionConcern.AssertArgumentNotEmpty(reSenha, "A confirmação de senha do usuário é obrigatória.");
+            AssertionConcern.AssertArgumentNotNull(Perfil, "O perfil do usuário é obrigatório.");
+            AssertionConcern.AssertArgumentPasswordStrenght(Senha, "A senha deve conter pelo menos 8 caracteres, incluindo letras maiúsculas, minúsculas, números e caracteres especiais.");
+            AssertionConcern.AssertArgumentPasswordStrenght(reSenha, "A senha deve conter pelo menos 8 caracteres, incluindo letras maiúsculas, minúsculas, números e caracteres especiais.");
+            AssertionConcern.AssertArgumentEquals(Senha, reSenha, "A senha e a confirmação de senha devem ser iguais.");
+            AssertionConcern.AssertArgumentEmailFormat(Email, "O email do usuário é inválido."); 
         }
     }
 }
