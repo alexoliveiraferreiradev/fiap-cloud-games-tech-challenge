1ee14f4f3a303c4ab47d87324fb67ac801541442
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-04-25 19:20:27 -0300

    cria uma classe de Mensagens de domínio visando a não repetição de string

---
+++ src/FiapCloundGames.API/Domain/Resources/MensagensDominio.cs
@@ -0,0 +1,28 @@
+﻿namespace FiapCloundGames.API.Domain.Resources
+{
+    public static class MensagensDominio
+    {
+        public static string UsuarioNomeObrigatorio = "O nome do usuário é obrigatório.";  
+        public static string UsuarioEmailObrigatorio = "O email do usuário é obrigatório.";
+        public static string UsuarioSenhaObrigatoria = "A senha do usuário é obrigatória.";
+        public static string UsuarioConfirmacaoSenhaObrigatoria = "A confirmação de senha do usuário é obrigatória.";
+        public static string UsuarioTamanhoNomeInvalido = "O nome do usuário deve conter entre 3 e 20 caracteres.";
+        public static string UsuarioSenhaFraca = "A senha deve conter pelo menos 8 caracteres, incluindo letras maiúsculas, minúsculas, números e caracteres especiais.";
+        public static string UsuarioSenhaConfirmacaoDiferente = "A senha e a confirmação de senha devem ser iguais.";
+        public static string UsuarioEmailInvalido = "O email do usuário é inválido.";
+        public static string UsuarioInativo = "O usuário deve estar ativo";    
+        public static string UsuarioEmailAntigoObrigatorio = "O email antigo do usuário é obrigatório.";  
+        public static string UsuarioEmailAntigoInvalido = "O email antigo do usuário é inválido.";
+        public static string UsuarioEmailNovoObrigatorio = "O novo email do usuário é obrigatório.";
+        public static string UsuarioEmailNovoInvalido = "O novo email do usuário é inválido.";
+        public static string UsuarioEmailAtualizaDiferente = "O novo email do usuário deve ser diferente do email antigo.";
+        public static string UsuarioSenhaAntigaObrigatoria = "A senha antiga do usuário é obrigatória.";
+        public static string UsuarioSenhaAntigaFraca = "A senha antiga do usuário é fraca.";
+        public static string UsuarioSenhaNovaObrigatoria = "A senha nova do usuário é obrigatória."; 
+        public static string UsuarioSenhaNovaFraca = "A senha nova do usuário é fraca.";
+        public static string PermissaoNegadaCriarAdministrador = "Permissão negada para criar um administrador.";
+        public static string UsuarioObrigatorio = "O usuário é obrigatório.";   
+        public static string UsuarioPerfilRebaixarInvalido = "O perfil do usuário não pode ser rebaixado, pois ele não é um administrador.";
+        public static string JogoNomeObrigatorio = "O nome do jogo é obrigatório.";
+    }
+}
