d55ca665828de6ea46c9f666d236d22760f30859
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-04-25 19:20:57 -0300

    adição do repositório IUsuarioService

---
+++ src/FiapCloundGames.API/Application/Services/Interfaces/IUsuarioService.cs
@@ -0,0 +1,11 @@
+﻿using FiapCloundGames.API.Domain.Entities;
+using FiapCloundGames.API.Infrastructure.Repository;
+
+namespace FiapCloundGames.API.Application.Services.Interfaces
+{
+    public interface IUsuarioService  
+    {
+        Usuario CriaAdministrador(Usuario entity, bool hasPermision, string token);
+        Usuario CriaJogador(Usuario entity);
+    }
+}
