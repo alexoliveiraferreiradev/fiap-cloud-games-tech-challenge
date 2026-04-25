4f9ee8f368e3218e508d7625f3e68b6eacff90ac
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-04-25 00:21:06 -0300

    cria um cenário de teste inicial para usuário

---
+++ tests/FiapCloundGames.UnitTests/Entities/UsuarioTests.cs
@@ -0,0 +1,25 @@
+﻿using FiapCloundGames.API.Domain.Entities;
+using FiapCloundGames.API.Domain.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FiapCloundGames.UnitTests.Entities
+{
+    public class UsuarioTests
+    {
+        [Fact(DisplayName = "Adicionar Novo Usuário")]
+        [Trait("Categoria", "Usuario Tests")]
+        public void AdicionarUsuario_UsuarioValido_DeveCriarComSucesso()
+        {
+            //Arrange
+            var usuario = new Usuario("TesteUser", "teste@teste.com", "senha123", "senha123", PerfilUsuario.Administrador, "123456789");
+            //Act 
+            var result = usuario.Validar(usuario);
+            //assert
+            Assert.True(result);  
+        }
+    }
+}
