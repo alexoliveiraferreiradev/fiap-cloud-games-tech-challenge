2753a7eff6a03c925e8a64e6190ebb3636639384
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-04-25 19:20:06 -0300

    adição de Assertions novas AssertArgumentLength,AssertArgumentNotEquals, AssertStateTrue

--- src/FiapCloundGames.API/Domain/Common/AssertionConcern.cs
+++ src/FiapCloundGames.API/Domain/Common/AssertionConcern.cs
@@ -23,6 +23,35 @@ namespace FiapCloundGames.API.Domain.Common
                 throw new DomainException(message);
             }
         }
+
+        /// <summary>
+        /// Valida se o comprimento de uma string está dentro do intervalo especificado (inclusivo),
+        /// e lança uma exceção de domínio com a mensagem fornecida caso esteja fora do intervalo.
+        /// </summary>
+        /// <param name="stringValue"></param>
+        /// <param name="minimum"></param>
+        /// <param name="maximum"></param>
+        /// <param name="message"></param>
+        /// <exception cref="DomainException"></exception>
+        public static void AssertArgumentLength(string stringValue, int minimum, int maximum, string message)
+        {
+            if (minimum > maximum)
+            {
+                throw new DomainException("valor mínimo deve ter menos ou igual ao valor máximo", nameof(minimum));
+            }
+
+            if (stringValue == null)
+            {
+                throw new DomainException(message);
+            }
+
+            int length = stringValue.Trim().Length;
+            if (length < minimum || length > maximum)
+            {
+                throw new DomainException(message);
+            }
+        }
+
         /// <summary>
         /// Valida se uma string é nula, vazia ou composta apenas por espaços em branco, e lança uma exceção de domínio com a mensagem fornecida caso seja.
         /// </summary>
@@ -64,6 +93,21 @@ namespace FiapCloundGames.API.Domain.Common
             }
         }
         /// <summary>
+        /// Valida se dois objetos são diferentes, utilizando o método Equals, e lança uma exceção de domínio com a mensagem fornecida caso sejam iguais.
+        /// </summary>
+        /// <param name="object1"></param>
+        /// <param name="object2"></param>
+        /// <param name="message"></param>
+        /// <exception cref="DomainException"></exception>
+        public static void AssertArgumentNotEquals(object object1, object object2, string message)
+        {
+            if (object1.Equals(object2))
+            {
+                throw new DomainException(message);
+            }
+        }
+
+        /// <summary>
         /// Valida a força da senha, verificando se ela tem pelo menos 8 caracteres, contém letras maiúsculas, minúsculas, números e caracteres especiais.
         /// </summary>
         /// <param name="password"></param>
@@ -97,7 +141,13 @@ namespace FiapCloundGames.API.Domain.Common
             }
 
         }
-        
+        public static void AssertStateTrue(bool boolValue, string message)
+        {
+            if (!boolValue)
+            {
+                throw new DomainException(message);
+            }
+        }
         /// <summary>
         /// Valida se o formato do email é válido, verificando se ele contém um endereço de email válido
         /// e não possui pontos consecutivos.
