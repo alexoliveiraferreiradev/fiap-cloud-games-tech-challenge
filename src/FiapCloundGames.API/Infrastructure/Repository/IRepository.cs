fd93d68d8931aa5b1e4b0008c7a0f75bad1fa212
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-04-25 19:21:14 -0300

    adição do repositório genérico IRepository

---
+++ src/FiapCloundGames.API/Infrastructure/Repository/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using FiapCloundGames.API.Domain.Common;
+
+namespace FiapCloundGames.API.Infrastructure.Repository
+{
+    public interface IRepository<T> where T : AgreggateRoot
+    {
+        Task Adicionar(T entity);   
+        Task Atualizar(T entity);   
+        Task Remover(T entity);
+        Task<T> ObterPorId(Guid id);   
+        Task<IEnumerable<T>> ObterTodos();  
+    }
+}
