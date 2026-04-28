54b294ec9ff006f62b32de7f071f12bd276dbf71
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-04-27 23:50:23 -0300

    adiciona classe de JogosService

---
+++ src/FiapCloundGames.API/Application/Services/JogosService.cs
@@ -0,0 +1,26 @@
+﻿using FiapCloundGames.API.Application.Dtos;
+using FiapCloundGames.API.Application.Services.Interfaces;
+using FiapCloundGames.API.Domain.Entities;
+using FiapCloundGames.API.Infrastructure.Repository;
+
+namespace FiapCloundGames.API.Application.Services
+{
+    public class JogosService : IJogosService
+    {
+        private readonly IJogosRepository _jogoRepository;
+        public JogosService(IJogosRepository jogoRepository) {
+            _jogoRepository = jogoRepository;   
+        }
+        public async Task<Jogos> CriaJogo(CriarJogoRequest request)
+        {
+            var jogos = new Jogos(request.Nome, request.Descricao, request.Preco, request.Genero);
+            await Adicionar(jogos);
+            return jogos;   
+        }
+
+        private async Task Adicionar(Jogos jogos)
+        {
+            await _jogoRepository.Adicionar(jogos); 
+        }
+    }
+}
