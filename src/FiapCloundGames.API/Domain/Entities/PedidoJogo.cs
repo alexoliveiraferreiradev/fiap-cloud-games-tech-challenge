47f33ee563cd318c9a3cebbe13c0a4d5d9e07997
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-04-27 23:53:48 -0300

    adiciona classe PedidoJogo

---
+++ src/FiapCloundGames.API/Domain/Entities/PedidoJogo.cs
@@ -0,0 +1,31 @@
+﻿using FiapCloundGames.API.Domain.Common;
+using FiapCloundGames.API.Domain.Resources;
+
+namespace FiapCloundGames.API.Domain.Entities
+{
+    public class PedidoJogo
+    {
+        public Guid JogoId { get; set; }
+        public string NomeJogo { get; set; }
+        public decimal PrecoNoMomento { get; set; }
+
+        protected PedidoJogo()
+        {
+        }
+
+        public PedidoJogo(Guid jogoId, string nomeJogo, decimal precoNoMomento)
+        {
+            ValidaEntidades(jogoId, nomeJogo, precoNoMomento);
+            JogoId = jogoId;
+            NomeJogo = nomeJogo;   
+            PrecoNoMomento = precoNoMomento;
+        }
+
+        private void ValidaEntidades(Guid jogoId, string nomeJogo, decimal precoNoMomento)
+        {
+            AssertionConcern.AssertArgumentNotNull(jogoId, MensagensDominio.JogoObrigatório);
+            AssertionConcern.AssertArgumentNotEmpty(nomeJogo, MensagensDominio.JogoNomeObrigatorio);  
+            AssertionConcern.AssertArgumentValueFormat(precoNoMomento, MensagensDominio.JogoPrecoInvalido);
+        }
+    }
+}
