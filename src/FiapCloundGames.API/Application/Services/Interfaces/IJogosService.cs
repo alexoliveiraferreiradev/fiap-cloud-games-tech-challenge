6da51a4ddcb978816b6fbbbe3cc92ba2f961abe6
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-04-27 23:49:33 -0300

    adiciona IJogosService

---
+++ src/FiapCloundGames.API/Application/Services/Interfaces/IJogosService.cs
@@ -0,0 +1,10 @@
+﻿using FiapCloundGames.API.Application.Dtos;
+using FiapCloundGames.API.Domain.Entities;
+
+namespace FiapCloundGames.API.Application.Services.Interfaces
+{
+    public interface IJogosService
+    {
+        Task<Jogos> CriaJogo(CriarJogoRequest request);
+    }
+}
