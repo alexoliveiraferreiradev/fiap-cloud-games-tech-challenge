7f1e139859f65526090bf897b1524d3c418ea28d
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-04-27 23:48:49 -0300

    adiciona dto de criação de jogo

---
+++ src/FiapCloundGames.API/Application/Dtos/CriarJogoRequest.cs
@@ -0,0 +1,6 @@
+﻿using FiapCloundGames.API.Domain.Enum;
+
+namespace FiapCloundGames.API.Application.Dtos
+{
+    public record CriarJogoRequest(string Nome, string Descricao, decimal Preco, GeneroJogo Genero);
+}
