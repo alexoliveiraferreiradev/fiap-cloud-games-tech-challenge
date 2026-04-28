914c489dd19926d710e58552dedd90d4f0e8e1a5
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-04-27 23:57:17 -0300

    Adiciona a fixture de Jogos

---
+++ tests/FiapCloundGames.UnitTests/Fixtures/JogosFixture.cs
@@ -0,0 +1,72 @@
+﻿using Bogus;
+using FiapCloundGames.API.Domain.Entities;
+using FiapCloundGames.API.Domain.Enum;
+
+namespace FiapCloundGames.UnitTests.Fixtures
+{
+    public class JogosFixture
+    {
+        private readonly Faker _faker;
+        public JogosFixture() { 
+            _faker = new Faker();
+        }
+
+        public Jogos ObtemJogosComSucesso()
+        {
+            var nomeJogo = _faker.Random.String(10);
+            var descricaoJogo = _faker.Random.String(50);
+            var precoJogo = _faker.Random.Decimal(10, 100);
+            GeneroJogo generoJogo = _faker.PickRandom<GeneroJogo>();
+            return new Jogos(nomeJogo, descricaoJogo, precoJogo, generoJogo);
+        }
+
+        public Jogos ObtemJogosNomeNaoPreenchido()
+        {
+            var nomeJogo = string.Empty;
+            var descricaoJogo = _faker.Random.String(50);
+            var precoJogo = _faker.Random.Decimal(10, 100);
+            GeneroJogo generoJogo = _faker.PickRandom<GeneroJogo>();
+            return new Jogos(nomeJogo, descricaoJogo, precoJogo, generoJogo);
+        }
+        public Jogos ObtemJogosDescricaoNaoPreenchido()
+        {
+            var nomeJogo = _faker.Random.String(10);
+            var descricaoJogo = string.Empty;
+            var precoJogo = _faker.Random.Decimal(10, 100);
+            GeneroJogo generoJogo = _faker.PickRandom<GeneroJogo>();
+            return new Jogos(nomeJogo, descricaoJogo, precoJogo, generoJogo);
+        }
+        public Jogos ObtemJogosNomeInvalido()
+        {
+            var nomeJogo = _faker.Random.String(21);
+            var descricaoJogo = _faker.Random.String(50);
+            var precoJogo = _faker.Random.Decimal(10, 100);
+            GeneroJogo generoJogo = _faker.PickRandom<GeneroJogo>();
+            return new Jogos(nomeJogo, descricaoJogo, precoJogo, generoJogo);
+        }
+        public Jogos ObtemJogosDescricaoInvalida()
+        {
+            var nomeJogo = _faker.Random.String(10);
+            var descricaoJogo = _faker.Random.String(101);
+            var precoJogo = _faker.Random.Decimal(10, 100);
+            GeneroJogo generoJogo = _faker.PickRandom<GeneroJogo>();
+            return new Jogos(nomeJogo, descricaoJogo, precoJogo, generoJogo);
+        }
+        public Jogos ObtemJogosPrecoInvalido()
+        {
+            var nomeJogo = _faker.Random.String(10);
+            var descricaoJogo = _faker.Random.String(100);
+            var precoJogo = _faker.Random.Decimal(-1, 0);
+            GeneroJogo generoJogo = _faker.PickRandom<GeneroJogo>();
+            return new Jogos(nomeJogo, descricaoJogo, precoJogo, generoJogo);
+        }
+        public Jogos ObtemJogosGeneroInvalido()
+        {
+            var nomeJogo = _faker.Random.String(10);
+            var descricaoJogo = _faker.Random.String(100);
+            var precoJogo = _faker.Random.Decimal(10, 100);
+            var generoJogo = (GeneroJogo)100;
+            return new Jogos(nomeJogo, descricaoJogo, precoJogo, generoJogo);
+        }
+    }
+}
