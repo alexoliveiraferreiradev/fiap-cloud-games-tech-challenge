a6de3c0a3f942ed4e71440ef3606c67dc71b6bcb
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-04-29 01:10:38 -0300

    modifica algumas mensagens

--- src/FiapCloundGames.API/Domain/Resources/MensagensDominio.cs
+++ src/FiapCloundGames.API/Domain/Resources/MensagensDominio.cs
@@ -3,33 +3,36 @@
     public static class MensagensDominio
     {
         public static string UsuarioNomeObrigatorio = "O nome do usuário é obrigatório.";
+        public static string UsuarioNomeAntigoObrigatorio = "O nome antigo do usuário é obrigatório.";
+        public static string UsuarioNomeNovoObrigatorio = "O novo nome do usuário é obrigatório.";        
         public static string UsuarioEmailObrigatorio = "O email do usuário é obrigatório.";
         public static string UsuarioSenhaObrigatoria = "A senha do usuário é obrigatória.";
         public static string UsuarioConfirmacaoSenhaObrigatoria = "A confirmação de senha do usuário é obrigatória.";
         public static string UsuarioTamanhoNomeInvalido = "O nome do usuário deve conter entre 3 e 20 caracteres.";
         public static string UsuarioSenhaFraca = "A senha deve conter pelo menos 8 caracteres, incluindo letras maiúsculas, minúsculas, números e caracteres especiais.";
-        public static string UsuarioSenhaConfirmacaoDiferente = "A senha e a confirmação de senha devem ser iguais.";
+        public static string UsuarioSenhaConfirmacaoDiferente = "A senha e a confirmação de senha devem ser iguais.";        
         public static string UsuarioEmailInvalido = "O email do usuário é inválido.";
         public static string UsuarioInativo = "O usuário deve estar ativo";
         public static string UsuarioJaExcluido = "O usuário já foi excluído.";
+        public static string UsuarioJaDesativado = "O usuário já foi desativado.";
         public static string UsuarioEmailAntigoObrigatorio = "O email antigo do usuário é obrigatório.";
         public static string UsuarioEmailAntigoInvalido = "O email antigo do usuário é inválido.";
         public static string UsuarioEmailNovoObrigatorio = "O novo email do usuário é obrigatório.";
-        public static string UsuarioEmailNovoInvalido = "O novo email do usuário é inválido.";
-        public static string UsuarioEmailAtualizaDiferente = "O novo email do usuário deve ser diferente do email antigo.";
+        public static string UsuarioEmailNovoInvalido = "O novo email do usuário é inválido.";        
         public static string UsuarioSenhaAntigaObrigatoria = "A senha antiga do usuário é obrigatória.";
         public static string UsuarioSenhaAntigaFraca = "A senha antiga do usuário é fraca.";
         public static string UsuarioSenhaNovaObrigatoria = "A senha nova do usuário é obrigatória.";
         public static string UsuarioSenhaNovaFraca = "A senha nova do usuário é fraca.";
         public static string PermissaoNegadaCriarAdministrador = "Permissão negada para criar um administrador.";
-        public static string UsuarioObrigatorio = "O usuário é obrigatório.";
+        public static string UsuarioNaoEncontrado = "O usuário não foi encontrado.";
         public static string UsuarioPerfilRebaixarInvalido = "O perfil do usuário não pode ser rebaixado, pois ele não é um administrador.";
         public static string JogoNomeObrigatorio = "O nome do jogo é obrigatório.";
         public static string JogoTamanhoNomeInvalido = "O nome do jogo deve conter entre 3 e 20 caracteres.";
         public static string JogoDescricaoTamanhoInvalido = "A descrição do jogo deve conter entre 5 e 100 caracteres.";
         public static string JogoDescricaoObrigatoria = "A descrição do jogo é obrigatória.";
         public static string JogoGeneroObrigatorio = "O gênero do jogo é inválido.";
-        public static string JogoObrigatório = "O jogo é obrigatório.";
+        public static string JogoNaoEncontrado = "O jogo não foi encontrado.";
         public static string JogoPrecoInvalido = "O preço do jogo deve ser um valor positivo."; 
+        public static string JogoInvalido = "O jogo já está desativado.";
     }
 }
