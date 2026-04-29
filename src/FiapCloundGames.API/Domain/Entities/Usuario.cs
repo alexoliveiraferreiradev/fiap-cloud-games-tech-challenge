d963542132f95df6c118df897dfa613f10465af2
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-04-29 01:10:23 -0300

    atualiza o usuário criando um método isolado para atualizar, além do método para desativar um usuário

--- src/FiapCloundGames.API/Domain/Entities/Usuario.cs
+++ src/FiapCloundGames.API/Domain/Entities/Usuario.cs
@@ -19,8 +19,8 @@ namespace FiapCloundGames.API.Domain.Entities
 
         public string Senha { get; private set; }
 
-        public TipoUsuario Perfil { get;private set; }
-        public bool Ativo {  get; private set; }
+        public TipoUsuario Perfil { get; private set; }
+        public bool Ativo { get; private set; }
         public DateTime DataCadastro { get; private set; }
         public DateTime DataAlteracao { get; private set; }
 
@@ -42,68 +42,87 @@ namespace FiapCloundGames.API.Domain.Entities
 
         public Usuario(string emailUsuario, string senhaUsuario)
         {
-            Email = emailUsuario;   
+            Email = emailUsuario;
             Senha = senhaUsuario;
             ValidarDadosEntradas();
         }
-               
+
         public void ValidarDadosEntradas()
         {
-            AssertionConcern.AssertArgumentNotEmpty(Email, MensagensDominio.UsuarioEmailObrigatorio);
-            AssertionConcern.AssertArgumentNotEmpty(Senha, MensagensDominio.UsuarioSenhaObrigatoria);
+            AssertionConcern.AssertArgumentEmpty(Email, MensagensDominio.UsuarioEmailObrigatorio);
+            AssertionConcern.AssertArgumentEmpty(Senha, MensagensDominio.UsuarioSenhaObrigatoria);
             AssertionConcern.AssertArgumentPasswordStrenght(Senha, MensagensDominio.UsuarioSenhaFraca);
             AssertionConcern.AssertArgumentEmailFormat(Email, MensagensDominio.UsuarioEmailInvalido);
             Ativo = true;
         }
-        
+
         public override void ValidarEntidade()
         {
-            AssertionConcern.AssertArgumentNotEmpty(NomeUsuario, MensagensDominio.UsuarioNomeObrigatorio);
-            AssertionConcern.AssertArgumentNotEmpty(Email, MensagensDominio.UsuarioEmailObrigatorio);
-            AssertionConcern.AssertArgumentNotEmpty(Senha, MensagensDominio.UsuarioSenhaObrigatoria);
-            AssertionConcern.AssertArgumentNotEmpty(confirmacaoSenha, MensagensDominio.UsuarioConfirmacaoSenhaObrigatoria);
+            AssertionConcern.AssertArgumentEmpty(NomeUsuario, MensagensDominio.UsuarioNomeObrigatorio);
+            AssertionConcern.AssertArgumentEmpty(Email, MensagensDominio.UsuarioEmailObrigatorio);
+            AssertionConcern.AssertArgumentEmpty(Senha, MensagensDominio.UsuarioSenhaObrigatoria);
+            AssertionConcern.AssertArgumentEmpty(confirmacaoSenha, MensagensDominio.UsuarioConfirmacaoSenhaObrigatoria);
             AssertionConcern.AssertArgumentLength(NomeUsuario, 3, 20, MensagensDominio.UsuarioTamanhoNomeInvalido);
-            AssertionConcern.AssertArgumentPasswordStrenght(Senha, MensagensDominio.UsuarioSenhaFraca);            
-            AssertionConcern.AssertArgumentEquals(Senha, confirmacaoSenha, MensagensDominio.UsuarioSenhaConfirmacaoDiferente);
-            AssertionConcern.AssertArgumentEmailFormat(Email, MensagensDominio.UsuarioEmailInvalido); 
+            AssertionConcern.AssertArgumentPasswordStrenght(Senha, MensagensDominio.UsuarioSenhaFraca);
+            AssertionConcern.AssertArgumentNotEquals(Senha, confirmacaoSenha, MensagensDominio.UsuarioSenhaConfirmacaoDiferente);
+            AssertionConcern.AssertArgumentEmailFormat(Email, MensagensDominio.UsuarioEmailInvalido);
         }
 
-        public void Deletar(string emailUsuario,string senhaUsuario)
+        public void Deletar(string emailUsuario, string senhaUsuario)
         {
-            if(!Ativo) throw new DomainException(MensagensDominio.UsuarioJaExcluido);            
-            if(string.IsNullOrEmpty(emailUsuario)) throw new DomainException(MensagensDominio.UsuarioEmailObrigatorio);
+            if (!Ativo) throw new DomainException(MensagensDominio.UsuarioJaExcluido);
+            if (string.IsNullOrEmpty(emailUsuario)) throw new DomainException(MensagensDominio.UsuarioEmailObrigatorio);
             if (Email != emailUsuario) throw new DomainException(MensagensDominio.UsuarioEmailInvalido);
-            if(string.IsNullOrEmpty(senhaUsuario)) throw new DomainException(MensagensDominio.UsuarioSenhaObrigatoria);
-           if(Senha != senhaUsuario) throw new DomainException(MensagensDominio.UsuarioSenhaFraca);
+            if (string.IsNullOrEmpty(senhaUsuario)) throw new DomainException(MensagensDominio.UsuarioSenhaObrigatoria);
+            if (Senha != senhaUsuario) throw new DomainException(MensagensDominio.UsuarioSenhaFraca);
             Ativo = false;
             DataAlteracao = DateTime.UtcNow;
         }
 
-        public void AtualizarEmail(string antigoEmail, string novoEmail)
+        public void Atualizar(string novoNome, string novoEmail, string novaSenha, string novaReSenha)
         {
-            AssertionConcern.AssertStateTrue(Ativo, MensagensDominio.UsuarioInativo);
-            AssertionConcern.AssertArgumentNotEmpty(antigoEmail, MensagensDominio.UsuarioEmailAntigoObrigatorio);
-            AssertionConcern.AssertArgumentNotEmpty(novoEmail, MensagensDominio.UsuarioEmailNovoObrigatorio);
-            AssertionConcern.AssertArgumentEmailFormat(antigoEmail, MensagensDominio.UsuarioEmailInvalido);
+            AssertionConcern.AssertStateFalse(Ativo, MensagensDominio.UsuarioInativo);
+
+            AtualizarNomeUsuario(novoNome);
+            AtualizarEmail(novoEmail);
+            AtualizarSenha(novaSenha, novaReSenha);
+        }
+
+        public void AtualizarNomeUsuario(string nomeNovo)
+        {
+            AssertionConcern.AssertArgumentEmpty(nomeNovo, MensagensDominio.UsuarioNomeNovoObrigatorio);
+            if (NomeUsuario == nomeNovo) return;
+            AssertionConcern.AssertArgumentLength(nomeNovo, 3, 20, MensagensDominio.UsuarioTamanhoNomeInvalido);
+            NomeUsuario = nomeNovo;
+        }
+
+        public void Desativar()
+        {
+            AssertionConcern.AssertStateFalse(Ativo, MensagensDominio.UsuarioJaDesativado);
+            Ativo = false;
+            DataAlteracao = DateTime.UtcNow;
+        }
+
+        public void AtualizarEmail(string novoEmail)
+        {
+            AssertionConcern.AssertArgumentEmpty(novoEmail, MensagensDominio.UsuarioEmailNovoObrigatorio);
+            if (Email == novoEmail) return;
             AssertionConcern.AssertArgumentEmailFormat(novoEmail, MensagensDominio.UsuarioEmailNovoInvalido);
-            AssertionConcern.AssertArgumentNotEquals(antigoEmail,novoEmail, MensagensDominio.UsuarioEmailAtualizaDiferente);
             Email = novoEmail;
         }
 
-        public void AtualizarSenha(string senhaAntiga, string novaSenha,string confirmacaoSenhaNova)
+        public void AtualizarSenha(string novaSenha, string confirmacaoSenhaNova)
         {
-            AssertionConcern.AssertStateTrue(Ativo, MensagensDominio.UsuarioInativo);
-            AssertionConcern.AssertArgumentNotEmpty(senhaAntiga, MensagensDominio.UsuarioSenhaAntigaObrigatoria);
-            AssertionConcern.AssertArgumentNotEmpty(novaSenha, MensagensDominio.UsuarioSenhaNovaObrigatoria);            
-            AssertionConcern.AssertArgumentPasswordStrenght(senhaAntiga, MensagensDominio.UsuarioSenhaFraca);
+            AssertionConcern.AssertArgumentEmpty(novaSenha, MensagensDominio.UsuarioSenhaNovaObrigatoria);
+            if (Senha == novaSenha) return;
             AssertionConcern.AssertArgumentPasswordStrenght(novaSenha, MensagensDominio.UsuarioSenhaNovaFraca);
-            AssertionConcern.AssertArgumentEquals(novaSenha, confirmacaoSenhaNova, MensagensDominio.UsuarioSenhaConfirmacaoDiferente);
+            AssertionConcern.AssertArgumentNotEquals(novaSenha, confirmacaoSenhaNova, MensagensDominio.UsuarioSenhaConfirmacaoDiferente);
             Senha = novaSenha;
         }
 
         public void RebaixarPerfil()
         {
-            if(Perfil!= TipoUsuario.Administrador)
+            if (Perfil != TipoUsuario.Administrador)
                 throw new DomainException(MensagensDominio.UsuarioPerfilRebaixarInvalido);
 
             Perfil = TipoUsuario.Jogador;
@@ -111,7 +130,7 @@ namespace FiapCloundGames.API.Domain.Entities
 
         public void PromoverPerfil(Usuario usuario)
         {
-            AssertionConcern.AssertArgumentNotNull(usuario, MensagensDominio.UsuarioObrigatorio);
+            AssertionConcern.AssertArgumentNotNull(usuario, MensagensDominio.UsuarioNaoEncontrado);
             typeof(Usuario).GetProperty("Perfil").SetValue(usuario, TipoUsuario.Administrador);
         }
     }
