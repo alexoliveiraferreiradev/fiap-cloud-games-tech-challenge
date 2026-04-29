384e9482d35416756aa05790293798b2cb338c35
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-04-29 11:47:41 -0300

    adiciona a referência a nova pasta de interfaces

--- src/FiapCloundGames.API/Domain/Common/AgreggateRoot.cs
+++ src/FiapCloundGames.API/Domain/Common/AgreggateRoot.cs
@@ -1,4 +1,6 @@
-﻿namespace FiapCloundGames.API.Domain.Common
+﻿using FiapCloundGames.API.Domain.Common.Interfaces;
+
+namespace FiapCloundGames.API.Domain.Common
 {
     public abstract class AgreggateRoot : EntityBase, IAggregateRoot
     {
