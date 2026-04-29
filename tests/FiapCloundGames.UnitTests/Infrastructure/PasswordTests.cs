0ccb75c709ac7229cbc9ff46b50726421a5117ec
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-04-29 14:36:40 -0300

    adiciona PasswordTests

---
+++ tests/FiapCloundGames.UnitTests/Infrastructure/PasswordTests.cs
@@ -0,0 +1,23 @@
+﻿using FiapCloundGames.API.Infrastructure.Security;
+
+namespace FiapCloundGames.UnitTests.Infrastructure
+{
+    public class PasswordTests
+    {
+        [Fact(DisplayName = "Deve gerar um hash BCrypt válido")]
+        [Trait("Categoria","Password Tests")]
+        public void Hash_DeveGerarStringCriptografada()
+        {
+            // Arrange - Classe REAL, sem Mock
+            var hasher = new PasswordHasher();
+            var senhaPura = "123456";
+
+            // Act
+            var hash = hasher.Hash(senhaPura);
+
+            // Assert
+            Assert.StartsWith("$2", hash); // Hashes BCrypt começam com $2a, $2b ou $2y
+            Assert.True(BCrypt.Net.BCrypt.Verify(senhaPura, hash));
+        }
+    }
+}
