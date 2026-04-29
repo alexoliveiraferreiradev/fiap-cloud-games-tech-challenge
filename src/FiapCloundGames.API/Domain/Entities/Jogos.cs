2faddc5ea3e7ddb7ab75db43163480a6f6829149
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-04-29 01:08:49 -0300

    adiciona métodos Desativar/Reativar/Atualizar/AtualizarNome/AtualizarDescricao/AtualizarPreco/AtualizarGenero. Adiciona propriedade DataAlteracao

--- src/FiapCloundGames.API/Domain/Entities/Jogos.cs
+++ src/FiapCloundGames.API/Domain/Entities/Jogos.cs
@@ -1,4 +1,5 @@
 ﻿using FiapCloundGames.API.Domain.Common;
+using FiapCloundGames.API.Domain.Common.Exceptions;
 using FiapCloundGames.API.Domain.Enum;
 using FiapCloundGames.API.Domain.Resources;
 
@@ -10,13 +11,13 @@ namespace FiapCloundGames.API.Domain.Entities
         public string Descricao { get; private set; }
         public decimal Preco { get; private set; }
         public bool Ativo { get; private set; }
-        public DateTime DataCadastro { get;private set; }
-        public GeneroJogo Genero { get; set; }
+        public DateTime DataCadastro { get; private set; }
+        public DateTime DataAlteracao { get; private set; }
+        public GeneroJogo Genero { get; private set; }
 
 
         protected Jogos()
         {
-
         }
 
 
@@ -31,14 +32,69 @@ namespace FiapCloundGames.API.Domain.Entities
 
         public override void ValidarEntidade()
         {
-            AssertionConcern.AssertArgumentNotEmpty(Nome, MensagensDominio.JogoNomeObrigatorio);
-            AssertionConcern.AssertArgumentNotEmpty(Descricao, MensagensDominio.JogoDescricaoObrigatoria);
+            AssertionConcern.AssertArgumentEmpty(Nome, MensagensDominio.JogoNomeObrigatorio);
+            AssertionConcern.AssertArgumentEmpty(Descricao, MensagensDominio.JogoDescricaoObrigatoria);
             AssertionConcern.AssertArgumentValueFormat(Preco, MensagensDominio.JogoPrecoInvalido);
             AssertionConcern.AssertArgumentLength(Nome, 3, 20, MensagensDominio.JogoTamanhoNomeInvalido);
             AssertionConcern.AssertArgumentLength(Descricao, 5, 100, MensagensDominio.JogoDescricaoTamanhoInvalido);
-            AssertionConcern.AssertArgumentRange((int)Genero,1,15, MensagensDominio.JogoGeneroObrigatorio);
+            AssertionConcern.AssertArgumentRange((int)Genero, 1, 15, MensagensDominio.JogoGeneroObrigatorio);
             Ativo = true;
             DataCadastro = DateTime.UtcNow;
+        }
+
+        public void Desativar()
+        {
+            AssertionConcern.AssertStateFalse(Ativo, MensagensDominio.JogoInvalido);
+            Ativo = false;
+            DataAlteracao = DateTime.UtcNow;
+        }
+
+        public void Reativar()
+        {
+            AssertionConcern.AssertStateTrue(!Ativo, MensagensDominio.JogoInvalido);
+            Ativo = true;
+            DataAlteracao = DateTime.UtcNow;
+        }
+
+        public void Atualizar(string novoNome, string novaDescricao, decimal novoPreco, GeneroJogo novoGenero)
+        {
+            AssertionConcern.AssertStateFalse(Ativo, MensagensDominio.JogoInvalido);
+
+            AtualizarNome(novoNome);
+            AtualizarDescricao(novaDescricao);
+            AtualizarPreco(novoPreco);
+            AtualizarGenero(novoGenero);
+            DataAlteracao = DateTime.UtcNow;    
+        }
+
+        private void AtualizarGenero(GeneroJogo novoGenero)
+        {
+            AssertionConcern.AssertArgumentRange((int)Genero, 1, 15, MensagensDominio.JogoGeneroObrigatorio);
+            if (Genero == novoGenero) return;
+            Genero = novoGenero;
+        }
+
+        private void AtualizarPreco(decimal novoPreco)
+        {
+            AssertionConcern.AssertArgumentValueFormat(novoPreco, MensagensDominio.JogoPrecoInvalido);
+            if (Preco == novoPreco) return;
+            Preco = novoPreco;
+        }
+
+        private void AtualizarDescricao(string novaDescricao)
+        {
+            AssertionConcern.AssertArgumentEmpty(novaDescricao, MensagensDominio.JogoDescricaoObrigatoria);
+            if (Descricao == novaDescricao) return;
+            AssertionConcern.AssertArgumentLength(novaDescricao, 5, 100, MensagensDominio.JogoDescricaoTamanhoInvalido);
+            Descricao = novaDescricao;
+        }
+
+        private void AtualizarNome(string novoNome)
+        {
+            AssertionConcern.AssertArgumentEmpty(novoNome, MensagensDominio.JogoNomeObrigatorio);
+            if (Nome == novoNome) return;
+            AssertionConcern.AssertArgumentLength(novoNome, 3, 20, MensagensDominio.JogoTamanhoNomeInvalido);
+            Nome = novoNome;
         }
     }
 }
