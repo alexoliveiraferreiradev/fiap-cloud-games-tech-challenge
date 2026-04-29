c09bbce919fe4e52fede0517840b1028a6fb2555
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-04-29 01:03:52 -0300

    adiciona o método AtualizarJogo

--- src/FiapCloundGames.API/Application/Services/Interfaces/IJogosService.cs
+++ src/FiapCloundGames.API/Application/Services/Interfaces/IJogosService.cs
@@ -1,4 +1,4 @@
-﻿using FiapCloundGames.API.Application.Dtos;
+﻿using FiapCloundGames.API.Application.Dtos.Jogos;
 using FiapCloundGames.API.Domain.Entities;
 
 namespace FiapCloundGames.API.Application.Services.Interfaces
@@ -6,5 +6,6 @@ namespace FiapCloundGames.API.Application.Services.Interfaces
     public interface IJogosService
     {
         Task<Jogos> CriaJogo(CriarJogoRequest request);
+        Task AtualizarJogo(Guid usuarioId,UpdateJogosRequest updateJogoRequest);
     }
 }
