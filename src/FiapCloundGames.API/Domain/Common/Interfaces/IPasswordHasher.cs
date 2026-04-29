63acb1273092246389e65c1129f6769419e1873a
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-04-29 11:49:23 -0300

    adiciona interface IPasswordHasher

---
+++ src/FiapCloundGames.API/Domain/Common/Interfaces/IPasswordHasher.cs
@@ -0,0 +1,9 @@
+﻿namespace FiapCloundGames.API.Domain.Common.Interfaces
+{
+    public interface IPasswordHasher
+    {
+        string Hash(string password);   
+        bool Verify(string password, string hashedPassoword);
+
+    }
+}
