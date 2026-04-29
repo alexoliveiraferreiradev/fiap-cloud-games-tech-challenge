ae0802dfb3a0317254366abcda2dba05f3694856
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-04-29 11:53:43 -0300

    adiciona a classe PassowordHasher,

---
+++ src/FiapCloundGames.API/Infrastructure/Security/PasswordHasher.cs
@@ -0,0 +1,17 @@
+﻿using FiapCloundGames.API.Domain.Common.Interfaces;
+
+namespace FiapCloundGames.API.Infrastructure.Security
+{
+    public class PasswordHasher : IPasswordHasher
+    {
+        public string Hash(string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Verify(string password, string hashedPassoword)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
