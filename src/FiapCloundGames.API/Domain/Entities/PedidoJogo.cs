1a80ac9b78b17f4b14923337868176fdff4981f2
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-04-30 17:22:52 -0300

    implementa o ValueObject Preco

--- src/FiapCloundGames.API/Domain/Entities/PedidoJogo.cs
+++ src/FiapCloundGames.API/Domain/Entities/PedidoJogo.cs
@@ -1,28 +1,29 @@
 ﻿using FiapCloundGames.API.Domain.Common;
 using FiapCloundGames.API.Domain.Resources;
+using FiapCloundGames.API.Domain.ValueObjects;
 
 namespace FiapCloundGames.API.Domain.Entities
 {
     public class PedidoJogo
     {
         public Guid JogoId { get; private set; }
-        public decimal PrecoNoMomento { get; private set; }
+        public Preco PrecoNoMomento { get; private set; }
 
         protected PedidoJogo()
         {
         }
 
-        public PedidoJogo(Guid jogoId, decimal precoNoMomento)
+        public PedidoJogo(Guid jogoId, Preco precoNoMomento)
         {
             ValidaEntidades(jogoId, precoNoMomento);
             JogoId = jogoId;
             PrecoNoMomento = precoNoMomento;
         }
 
-        private void ValidaEntidades(Guid jogoId, decimal precoNoMomento)
+        private void ValidaEntidades(Guid jogoId,Preco preco)
         {
             AssertionConcern.AssertArgumentNotNull(jogoId, MensagensDominio.JogoNaoEncontrado);
-            AssertionConcern.AssertArgumentValueFormat(precoNoMomento, MensagensDominio.ValorInvalido);
+            AssertionConcern.AssertArgumentNotNull(preco, MensagensDominio.PrecoObrigatorio);
         }
     }
 }
