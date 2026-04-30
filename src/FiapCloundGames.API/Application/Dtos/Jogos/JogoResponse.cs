de39baaaab81215e3bf95596b7c696a79f1edb11
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-04-30 15:39:27 -0300

    adiciona a classe dto JogoResponse

---
+++ src/FiapCloundGames.API/Application/Dtos/Jogos/JogoResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace FiapCloundGames.API.Application.Dtos.Jogos
+{
+    public class JogoResponse
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public decimal PrecoOriginal { get; set; }
+        public decimal PrecoAtual { get; set; }
+        public bool TemDesconto => PrecoAtual < PrecoOriginal;
+    }
+}
