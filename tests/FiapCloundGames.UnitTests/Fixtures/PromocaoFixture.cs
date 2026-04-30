39331198b083a71337359e75134f6f740eff3cb1
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-04-30 14:30:50 -0300

    adiciona método ObtemPromacaoRequest

--- tests/FiapCloundGames.UnitTests/Fixtures/PromocaoFixture.cs
+++ tests/FiapCloundGames.UnitTests/Fixtures/PromocaoFixture.cs
@@ -1,6 +1,17 @@
-﻿namespace FiapCloundGames.UnitTests.Fixtures
+﻿using FiapCloundGames.API.Application.Dtos.Promocao;
+
+namespace FiapCloundGames.UnitTests.Fixtures
 {
     public class PromocaoFixture
-    {
+    {       
+        public PromocaoFixture()
+        {
+       
+        }
+
+        public CriaPromocaoRequest ObtemPromacaoRequest(Guid jogoId)
+        {
+            return new CriaPromocaoRequest(jogoId, 90.00m, DateTime.UtcNow.AddDays(10));
+        }
     }
 }
