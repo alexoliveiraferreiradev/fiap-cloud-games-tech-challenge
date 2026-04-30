16d74c0e9b7abec9a8ec23adfad92c6ec4da407f
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-04-30 00:49:52 -0300

    adiciona classe de Promocao, assim como os métodos ValidarEntidade, EstaValida, Desativar e AtualizarPrecoPromocional

---
+++ src/FiapCloundGames.API/Domain/Entities/Promocao.cs
@@ -0,0 +1,43 @@
+﻿using FiapCloundGames.API.Domain.Common;
+using FiapCloundGames.API.Domain.Common.Exceptions;
+using FiapCloundGames.API.Domain.Resources;
+
+namespace FiapCloundGames.API.Domain.Entities
+{
+    public class Promocao : EntityBase
+    {
+        public Promocao(Guid jogoId, decimal valorPromocao, DateTime dataFim)
+        {
+            JogoId = jogoId;    
+            Valor = valorPromocao;
+            Ativo = true;
+            DataInicio = DateTime.UtcNow;
+            DataFim = dataFim;
+        }
+        protected Promocao() { }
+        public Guid JogoId { get; private set; }
+        public decimal Valor { get; private set; }
+        public bool Ativo { get; private set; }
+        public DateTime DataInicio { get; private set; }
+        public DateTime DataFim { get; private set; }
+
+        public override void ValidarEntidade()
+        {
+            AssertionConcern.AssertArgumentNotNull(JogoId, MensagensDominio.JogoNaoEncontrado);
+            AssertionConcern.AssertArgumentValueFormat(Valor, MensagensDominio.PromocaoValorInvalido);
+            if (DataFim <= DateTime.UtcNow) throw new DomainException(MensagensDominio.PromocaoDataFimInvalida);
+        }
+
+        public bool EstaValida()=>
+            Ativo && DateTime.UtcNow >= DataInicio && DateTime.UtcNow <= DataFim;
+
+        public void Desativar() => Ativo = false;
+
+        public void AtualizarPrecoPromocional(decimal novoValor)
+        {
+            AssertionConcern.AssertArgumentValueFormat(novoValor, MensagensDominio.PromocaoValorInvalido);
+            Valor = novoValor;
+        }
+
+    }
+}
