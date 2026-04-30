a57362078b4b0d9befaff6b39612dec19d1299cb
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-04-30 16:38:06 -0300

    adiciona Preço na ValueObject

---
+++ src/FiapCloundGames.API/Domain/ValueObjects/Preco.cs
@@ -0,0 +1,20 @@
+﻿using FiapCloundGames.API.Domain.Common;
+using FiapCloundGames.API.Domain.Resources;
+
+namespace FiapCloundGames.API.Domain.ValueObjects
+{
+    public class Preco : ValueObject
+    {
+        public decimal Valor { get; }
+
+        public Preco(decimal valor)
+        {
+            AssertionConcern.AssertArgumentValueFormat(valor, MensagensDominio.ValorInvalido);
+            Valor = valor;  
+        }
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return Valor;
+        }
+    }
+}
