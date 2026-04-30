e0667a4dc6e36d043575b0eca16e4c646b3794c5
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-04-29 22:09:15 -0300

    adiciona o enum para motivos de deleção de um usuário

---
+++ src/FiapCloundGames.API/Domain/Enum/Motivo.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+
+namespace FiapCloundGames.API.Domain.Enum
+{
+    public enum Motivo
+    {
+        [Description("Solicitado pelo usuário")]
+        SolicitacaoDoUsuario = 1,
+        [Description("Inatividade prolongada")]
+        Inatividade = 2,
+        [Description("Violação dos Termos de Uso")]
+        ViolacaoTermos = 3,
+        [Description("Comportamento tóxico ou inadequado")]
+        ComportamentoInadequado = 4,
+        [Description("Uso de trapaças ou softwares de terceiros (Cheat/Bot)")]
+        FraudeOuTrapaca = 5,
+        [Description("Duplicidade de conta")]
+        ContaDuplicada = 6,
+        [Description("Outros motivos")]
+        Outros = 99
+    }
+}
