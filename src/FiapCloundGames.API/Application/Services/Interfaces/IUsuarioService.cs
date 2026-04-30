103ec0a3488fe61eb950e7e97fdbd49cb82e3ab7
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-04-29 22:32:25 -0300

    no DesativarUsuario usa o DeleteUsuarioRequest como parâmetro

--- src/FiapCloundGames.API/Application/Services/Interfaces/IUsuarioService.cs
+++ src/FiapCloundGames.API/Application/Services/Interfaces/IUsuarioService.cs
@@ -1,5 +1,6 @@
 ﻿using FiapCloundGames.API.Application.Dtos.Usuario;
 using FiapCloundGames.API.Domain.Entities;
+using FiapCloundGames.API.Domain.Enum;
 
 namespace FiapCloundGames.API.Application.Services.Interfaces
 {
@@ -8,7 +9,7 @@ namespace FiapCloundGames.API.Application.Services.Interfaces
         Task<Usuario> CadastrarAdministrador(CriaUsuarioRequest request, bool hasPermision, string token);
         Task<Usuario> CadastrarJogador(CriaUsuarioRequest request);
         Task AtualizarUsuario(Guid usuarioId, UpdateUsuarioRequest updateUsuarioRequest);
-        Task DesativarUsuario(Guid usuarioId);
+        Task DesativarUsuario(DeleteUsuarioRequest deleteUsuarioRequest);
         Task<Usuario> Autenticar(LoginRequest loginRequest);
 
     }
