5bb288cae0b3fc642c371d6e266166fbc1cb8775
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-04-30 16:06:19 -0300

    adiciona método ObtemJogosAtivos

--- src/FiapCloundGames.API/Infrastructure/Repository/IJogosRepository.cs
+++ src/FiapCloundGames.API/Infrastructure/Repository/IJogosRepository.cs
@@ -5,6 +5,7 @@ namespace FiapCloundGames.API.Infrastructure.Repository
     public interface IJogosRepository : IRepository<Jogos>
     {
         Task<Jogos> ObtemPorNome(string nomeJogo);
+        Task<IEnumerable<Jogos>> ObtemJogosAtivos();
         
     }
 }
