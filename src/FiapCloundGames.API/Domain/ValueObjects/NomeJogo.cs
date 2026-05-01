5e6a46242137e928b5f375da5ecbd0d1e282d041
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-05-01 11:33:50 -0300

    cria ValueObject NomeJogo

---
+++ src/FiapCloundGames.API/Domain/ValueObjects/NomeJogo.cs
@@ -0,0 +1,26 @@
+﻿using FiapCloundGames.API.Domain.Common;
+using FiapCloundGames.API.Domain.Resources;
+
+namespace FiapCloundGames.API.Domain.ValueObjects
+{
+    public class NomeJogo : ValueObject<NomeJogo>
+    {
+        public string Valor { get; }
+
+        public NomeJogo(string nomeJogo)
+        {
+            AssertionConcern.AssertArgumentEmpty(nomeJogo, MensagensDominio.JogoNomeObrigatorio);
+            AssertionConcern.AssertArgumentLength(nomeJogo, 3, 40, MensagensDominio.JogoTamanhoNomeInvalido);
+            Valor = nomeJogo;
+        }
+        protected override bool EqualsCore(NomeJogo other)
+        {
+            return Valor == other.Valor;    
+        }
+
+        protected override int GetHashCodeCore()
+        {
+            return Valor.GetHashCode();
+        }
+    }
+}
