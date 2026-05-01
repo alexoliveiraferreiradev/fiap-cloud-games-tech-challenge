3c7b33bb4819041146d7dd3ddba05ded134c4770
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-05-01 03:06:22 -0300

    adiciona construtor com validações

--- src/FiapCloundGames.API/Domain/ValueObjects/Senha.cs
+++ src/FiapCloundGames.API/Domain/ValueObjects/Senha.cs
@@ -1,10 +1,19 @@
 ﻿using FiapCloundGames.API.Domain.Common;
+using FiapCloundGames.API.Domain.Resources;
 
 namespace FiapCloundGames.API.Domain.ValueObjects
 {
     public class Senha : ValueObject<Senha>
     {
         public string Hash { get; }
+
+        public Senha(string senha)
+        {
+            AssertionConcern.AssertArgumentEmpty(senha, MensagensDominio.UsuarioSenhaObrigatoria);
+            AssertionConcern.AssertArgumentPasswordStrenght(senha, MensagensDominio.UsuarioSenhaFraca);
+            Hash = senha;
+        }
+
         protected override bool EqualsCore(Senha other)
         {
             return Hash == other.Hash;    
