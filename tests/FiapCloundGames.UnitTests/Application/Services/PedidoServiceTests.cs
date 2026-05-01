fd5d51927f3f3e92cf1c8e297095a7fbc1005a7a
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-05-01 15:22:52 -0300

    cria teste CriaPedido_PedidoValido_DeveCriarComSucesso

--- tests/FiapCloundGames.UnitTests/Application/Services/PedidoServiceTests.cs
+++ tests/FiapCloundGames.UnitTests/Application/Services/PedidoServiceTests.cs
@@ -2,5 +2,11 @@
 {
     public class PedidoServiceTests
     {
+        [Fact(DisplayName ="Sucesso ao criar pedido - pedido criado com sucesso")]
+        [Trait("Categoria","Pedido Service Tests")]
+        public void CriaPedido_PedidoValido_DeveCriarComSucesso()
+        {
+
+        }
     }
 }
