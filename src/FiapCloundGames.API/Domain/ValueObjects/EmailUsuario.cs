744fcf2cb8c8822255961a80f934d922d7fcd574
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-05-01 00:19:52 -0300

    adiciona ValueObject EmailUsuario

---
+++ src/FiapCloundGames.API/Domain/ValueObjects/EmailUsuario.cs
@@ -0,0 +1,21 @@
+﻿using FiapCloundGames.API.Domain.Common;
+using FiapCloundGames.API.Domain.Resources;
+
+namespace FiapCloundGames.API.Domain.ValueObjects
+{
+    public class EmailUsuario : ValueObject
+    {
+        public string Email { get;  }
+
+        public EmailUsuario(string emailUsuario)
+        {
+            AssertionConcern.AssertArgumentEmpty(emailUsuario, MensagensDominio.UsuarioEmailObrigatorio);
+            AssertionConcern.AssertArgumentEmailFormat(emailUsuario, MensagensDominio.EmailInvalido);
+            Email = emailUsuario;   
+        }
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return Email;
+        }
+    }
+}
