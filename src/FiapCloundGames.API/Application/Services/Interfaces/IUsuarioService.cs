77078668763cec24323dd40f37e6488866ee410f
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-05-01 04:06:51 -0300

    altera nome do contrato de CadastrarJogador para CadastrarUsuario

--- src/FiapCloundGames.API/Application/Services/Interfaces/IUsuarioService.cs
+++ src/FiapCloundGames.API/Application/Services/Interfaces/IUsuarioService.cs
@@ -7,7 +7,7 @@ namespace FiapCloundGames.API.Application.Services.Interfaces
     public interface IUsuarioService  
     {
         Task<Usuario> CadastrarAdministrador(CriaUsuarioRequest request, bool hasPermision, string token);
-        Task<Usuario> CadastrarJogador(CriaUsuarioRequest request);
+        Task<Usuario> CadastrarUsuario(CriaUsuarioRequest request);
         Task AtualizarUsuario(Guid usuarioId, UpdateUsuarioRequest updateUsuarioRequest);
         Task Desativar(DeleteUsuarioRequest deleteUsuarioRequest);
         Task Reativar(Guid usuarioId);
