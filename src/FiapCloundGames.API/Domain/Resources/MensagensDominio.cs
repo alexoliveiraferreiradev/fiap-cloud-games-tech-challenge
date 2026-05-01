c38254cae19204736fe1e72797b266b2440c6ae4
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-05-01 00:14:32 -0300

    adiciona region de ValueObject e adiciona mensagem EmailObrigatorio

--- src/FiapCloundGames.API/Domain/Resources/MensagensDominio.cs
+++ src/FiapCloundGames.API/Domain/Resources/MensagensDominio.cs
@@ -5,13 +5,13 @@
         #region Usuario
         public static string UsuarioNomeObrigatorio = "O nome do usuário é obrigatório.";
         public static string UsuarioNomeAntigoObrigatorio = "O nome antigo do usuário é obrigatório.";
-        public static string UsuarioNomeNovoObrigatorio = "O novo nome do usuário é obrigatório.";        
+        public static string UsuarioNomeNovoObrigatorio = "O novo nome do usuário é obrigatório.";
         public static string UsuarioEmailObrigatorio = "O email do usuário é obrigatório.";
         public static string UsuarioSenhaObrigatoria = "A senha do usuário é obrigatória.";
         public static string UsuarioConfirmacaoSenhaObrigatoria = "A confirmação de senha do usuário é obrigatória.";
         public static string UsuarioTamanhoNomeInvalido = "O nome do usuário deve conter entre 3 e 20 caracteres.";
         public static string UsuarioSenhaFraca = "A senha deve conter pelo menos 8 caracteres, incluindo letras maiúsculas, minúsculas, números e caracteres especiais.";
-        public static string UsuarioSenhaConfirmacaoDiferente = "A senha e a confirmação de senha devem ser iguais.";        
+        public static string UsuarioSenhaConfirmacaoDiferente = "A senha e a confirmação de senha devem ser iguais.";
         public static string UsuarioEmailInvalido = "O email do usuário é inválido.";
         public static string UsuarioInativo = "O usuário deve estar ativo";
         public static string UsuarioAtivo = "O usuário deve estar inativo";
@@ -19,7 +19,7 @@
         public static string UsuarioEmailAntigoObrigatorio = "O email antigo do usuário é obrigatório.";
         public static string UsuarioEmailAntigoInvalido = "O email antigo do usuário é inválido.";
         public static string UsuarioEmailNovoObrigatorio = "O novo email do usuário é obrigatório.";
-        public static string UsuarioEmailNovoInvalido = "O novo email do usuário é inválido.";        
+        public static string UsuarioEmailNovoInvalido = "O novo email do usuário é inválido.";
         public static string UsuarioSenhaAntigaObrigatoria = "A senha antiga do usuário é obrigatória.";
         public static string UsuarioSenhaAntigaFraca = "A senha antiga do usuário é fraca.";
         public static string UsuarioSenhaNovaObrigatoria = "A senha nova do usuário é obrigatória.";
@@ -57,7 +57,10 @@
         public static string PromocaoInativa = "A promoção já está desativada.";
         #endregion
 
+        #region Value Objects
         public static string ValorInvalido = "O valor deve ser positivo.";
         public static string PrecoObrigatorio = "O preço é obrigatório.";
+        public static string EmailObrigatorio = "O email do usuário é obrigatório.";
+        #endregion
     }
 }
