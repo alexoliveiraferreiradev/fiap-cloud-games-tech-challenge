fbd03445a94d9391bcb3bb551bf677a6f4637853
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-05-01 02:58:47 -0300

    adiciona o ValueObject de Senha

---
+++ src/FiapCloundGames.API/Domain/ValueObjects/Senha.cs
@@ -0,0 +1,18 @@
+﻿using FiapCloundGames.API.Domain.Common;
+
+namespace FiapCloundGames.API.Domain.ValueObjects
+{
+    public class Senha : ValueObject<Senha>
+    {
+        public string Hash { get; }
+        protected override bool EqualsCore(Senha other)
+        {
+            return Hash == other.Hash;    
+        }
+
+        protected override int GetHashCodeCore()
+        {
+            return Hash.GetHashCode();
+        }
+    }
+}
