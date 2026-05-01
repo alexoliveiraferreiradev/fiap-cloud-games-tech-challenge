dc7f12019df9e942948809a0b98207b490abac72
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-05-01 14:54:06 -0300

    adiciona PeriodoTests

---
+++ tests/FiapCloundGames.UnitTests/Domain/ValueObjects/PeriodoTests.cs
@@ -0,0 +1,35 @@
+﻿using FiapCloundGames.API.Domain.Common.Exceptions;
+using FiapCloundGames.API.Domain.Resources;
+using FiapCloundGames.API.Domain.ValueObjects;
+
+namespace FiapCloundGames.UnitTests.Domain.ValueObjects
+{
+    public class PeriodoTests
+    {
+        [Fact(DisplayName = "Sucesso ao criar período - deve criar com sucesso")]
+        [Trait("Categoria","Periodo Tests")]
+        public void CriaPeriodo_PeriodoValido_DeveCriarComSucesso()
+        {
+            //Arrange
+            var dataFim = DateTime.UtcNow.AddDays(10);
+            //Assert
+            var periodoVO = new Periodo(dataFim);
+            //Act
+            Assert.True(periodoVO.DataFim> periodoVO.DataInicio);
+            Assert.Equal(dataFim, periodoVO.DataFim);
+        }
+
+        [Fact(DisplayName = "Falha ao criar período - data final menor")]
+        [Trait("Categoria","Periodo Tests")]
+        public void CriaPeriodo_PeriodoInvalido_DeveLancarExcecao()
+        {
+            //Arrange
+            var dataFim = DateTime.UtcNow.AddDays(-10);
+            //Assert
+            var result = Assert.Throws<DomainException>(() => new Periodo(dataFim));
+            //Act
+            Assert.Equal(MensagensDominio.DataFimInvalida, result.Message);
+        }
+
+    }
+}
