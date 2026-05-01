d3921814332795806d567f231ae0498c4bdca5b4
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-05-01 12:38:08 -0300

    altera método do ObtemJogosGeneroInvalido()

--- tests/FiapCloundGames.UnitTests/Fixtures/JogosFixture.cs
+++ tests/FiapCloundGames.UnitTests/Fixtures/JogosFixture.cs
@@ -35,47 +35,14 @@ namespace FiapCloundGames.UnitTests.Fixtures
             typeof(Jogos).GetProperty("Ativo").SetValue(jogos, false);
             return jogos;
         }
-
        
-        public Jogos ObtemJogosDescricaoNaoPreenchido()
-        {
-            var nomeJogo = _faker.Random.String(10);
-            var descricaoJogo = string.Empty;
-            var precoJogo = new Preco( _faker.Random.Decimal(10, 100));
-            GeneroJogo generoJogo = _faker.PickRandom<GeneroJogo>();
-            return new Jogos(nomeJogo, descricaoJogo, precoJogo, generoJogo);
-        }
-        public Jogos ObtemJogosNomeInvalido()
-        {
-            var nomeJogo = _faker.Random.String(21);
-            var descricaoJogo = _faker.Random.String(50);
-            var precoJogo = new Preco( _faker.Random.Decimal(10, 100));
-            GeneroJogo generoJogo = _faker.PickRandom<GeneroJogo>();
-            return new Jogos(nomeJogo, descricaoJogo, precoJogo, generoJogo);
-        }
-        public Jogos ObtemJogosDescricaoInvalida()
-        {
-            var nomeJogo = _faker.Random.String(10);
-            var descricaoJogo = _faker.Random.String(101);
-            var precoJogo = new Preco(_faker.Random.Decimal(10, 100));
-            GeneroJogo generoJogo = _faker.PickRandom<GeneroJogo>();
-            return new Jogos(nomeJogo, descricaoJogo, precoJogo, generoJogo);
-        }
-        public Jogos ObtemJogosPrecoInvalido()
-        {
-            var nomeJogo = _faker.Random.String(10);
-            var descricaoJogo = _faker.Random.String(100);
-            var precoJogo = new Preco(_faker.Random.Decimal(-1, 0));
-            GeneroJogo generoJogo = _faker.PickRandom<GeneroJogo>();
-            return new Jogos(nomeJogo, descricaoJogo, precoJogo, generoJogo);
-        }
         public Jogos ObtemJogosGeneroInvalido()
         {
             var nomeJogo = _faker.Random.String(10);
             var descricaoJogo = _faker.Random.String(100);
             var precoJogo = new Preco( _faker.Random.Decimal(10, 100));
             var generoJogo = (GeneroJogo)100;
-            return new Jogos(nomeJogo, descricaoJogo, precoJogo, generoJogo);
+            return new Jogos(_nomeJogo, _descricaoJogo, _precoJogo, generoJogo);
         }
     }
 }
