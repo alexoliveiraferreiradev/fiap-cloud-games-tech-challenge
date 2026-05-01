7fc7c6c18bbcd7ddb354665977fae4fe60d300b6
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-05-01 15:36:46 -0300

    aplica lógica Realiza pedido com sucesso

--- tests/FiapCloundGames.UnitTests/Application/Services/PedidoServiceTests.cs
+++ tests/FiapCloundGames.UnitTests/Application/Services/PedidoServiceTests.cs
@@ -1,12 +1,37 @@
-﻿namespace FiapCloundGames.UnitTests.Application.Services
+﻿using FiapCloundGames.API.Domain.Entities;
+using FiapCloundGames.API.Infrastructure.Repository;
+using FiapCloundGames.UnitTests.Fixtures;
+using Moq;
+
+namespace FiapCloundGames.UnitTests.Application.Services
 {
     public class PedidoServiceTests
     {
+        private JogosFixture _jogosFixture;
+        private UsuarioFixture _usuarioFixture;
+        public PedidoServiceTests()
+        {
+            _jogosFixture = new JogosFixture();
+            _usuarioFixture = new UsuarioFixture();
+        }
         [Fact(DisplayName ="Sucesso ao criar pedido - pedido criado com sucesso")]
         [Trait("Categoria","Pedido Service Tests")]
-        public void CriaPedido_PedidoValido_DeveCriarComSucesso()
+        public async Task RealizaPedido_PedidoValido_DeveCriarComSucesso()
         {
-
+            //Arrange
+            var usuario = _usuarioFixture.ObtemJogadorComSucesso();
+            var jogoPedido = _jogosFixture.ObtemJogosComSucesso();
+            List<Guid> lista = new List<Guid>();
+            //Mock
+            var usuarioMock = new Mock<IUsuarioRepository>();
+            var pedidoMock = new Mock<IPedidoRepository>();
+            usuarioMock.Setup(r => r.ObterPorId(usuario.Id)).ReturnsAsync(usuario);
+            var pedidoService = new PedidoService(usuarioMock.Object, pedidoMock.Object);
+            //Act
+            lista.Add(jogoPedido.Id);
+            await pedidoService.RealizarPedido(usuario.Id, lista);
+            //Assert
+            pedidoMock.Verify(p => p.Adicionar(It.IsAny<Pedido>), Times.Once);
         }
     }
 }
