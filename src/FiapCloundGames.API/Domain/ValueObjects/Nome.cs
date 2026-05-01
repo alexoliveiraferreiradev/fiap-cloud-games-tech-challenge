c51c532b4815e4bb84a6326716e1727c22b42a6e
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-05-01 01:20:24 -0300

    adiciona VO Nome

---
+++ src/FiapCloundGames.API/Domain/ValueObjects/Nome.cs
@@ -0,0 +1,20 @@
+﻿using FiapCloundGames.API.Domain.Common;
+using FiapCloundGames.API.Domain.Resources;
+
+namespace FiapCloundGames.API.Domain.ValueObjects
+{
+    public class Nome : ValueObject
+    {
+        public string Valor { get; }
+        public Nome(string nomeUsuario)
+        {
+            AssertionConcern.AssertArgumentEmpty(nomeUsuario, MensagensDominio.UsuarioNomeObrigatorio);
+            AssertionConcern.AssertArgumentLength(nomeUsuario, 3, 20, MensagensDominio.UsuarioTamanhoNomeInvalido);
+            Valor = nomeUsuario;
+        }
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return Valor;
+        }
+    }
+}
