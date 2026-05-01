4d8f9e24994e75ae2d253007ab0ae0c2f33a4651
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-05-01 11:40:22 -0300

    cria ValueObject de Descricao

---
+++ src/FiapCloundGames.API/Domain/ValueObjects/Descricao.cs
@@ -0,0 +1,27 @@
+﻿using FiapCloundGames.API.Domain.Common;
+using FiapCloundGames.API.Domain.Resources;
+
+namespace FiapCloundGames.API.Domain.ValueObjects
+{
+    public class Descricao : ValueObject<Descricao>
+    {
+        public string Valor { get; set; }
+
+        public Descricao(string descricao)
+        {
+            AssertionConcern.AssertArgumentEmpty(descricao, MensagensDominio.JogoDescricaoObrigatoria);
+            AssertionConcern.AssertArgumentLength(descricao, 5, 100, MensagensDominio.JogoDescricaoTamanhoInvalido);
+            Valor = descricao;
+        }
+
+        protected override bool EqualsCore(Descricao other)
+        {
+            return Valor == other.Valor;    
+        }
+
+        protected override int GetHashCodeCore()
+        {
+            return Valor.GetHashCode();
+        }
+    }
+}
