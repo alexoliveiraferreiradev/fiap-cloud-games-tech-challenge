6c654d708ee8ceb056ab43af27ea19be6346d292
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-05-01 12:25:00 -0300

    altera lógica do teste CriaDescricao_DescricaoNoLimite_DevePassar e cria teste CriaDescricao_DescricaoValida_DevePassar

--- tests/FiapCloundGames.UnitTests/Domain/ValueObjects/DescricaoTests.cs
+++ tests/FiapCloundGames.UnitTests/Domain/ValueObjects/DescricaoTests.cs
@@ -15,18 +15,28 @@ namespace FiapCloundGames.UnitTests.Domain.ValueObjects
         {
             _faker = new Faker();   
         }
+        [Fact(DisplayName = "Sucesso ao criar descricao jogo - cria descricao com sucesso")]
+        [Trait("Categoria", "Descricao Tests")]
+        public void CriaDescricao_DescricaoValida_DevePassar()
+        {
+            //Act
+            var descricaoJogo = "Testando descrição do jogo";
+            //Arrange
+            var descricaoJogoVO = new Descricao(descricaoJogo);
+            //Assert
+            Assert.Equal(descricaoJogo, descricaoJogoVO.Valor);
+        }
         [Fact(DisplayName = "Sucesso ao criar descricao jogo - descrição no limite")]
         [Trait("Categoria", "Descricao Tests")]
         public void CriaDescricao_DescricaoNoLimite_DevePassar()
         {
             //Act
             var nomeJogoVO = new NomeJogo("Teste Jogo");
-            var descricaoNoLimiteVO = new Descricao( _faker.Random.String(100));
             var precoVO = new Preco(10.0m);
             //Arrange
-            var jogo = new Jogos(nomeJogoVO, descricaoNoLimiteVO, precoVO, GeneroJogo.Acao);
+            var descricaoNoLimite = new Descricao(_faker.Random.String(100));
             //Assert
-            Assert.Equal(100, jogo.Descricao.Valor.Length);
+            Assert.Equal(100, descricaoNoLimite.Valor.Length);
         }
 
         [Fact(DisplayName = "Falha ao criar descrição jogo - descrição do jogo inválida deve lançar exceção")]
@@ -39,6 +49,19 @@ namespace FiapCloundGames.UnitTests.Domain.ValueObjects
             var result = Assert.Throws<DomainException>(() => new Descricao(novaDescricao));
             //Assert
             Assert.Equal(MensagensDominio.JogoDescricaoTamanhoInvalido, result.Message);
+        }
+
+
+        [Fact(DisplayName = "Falha ao criar descrição do jogo - descrição do jogo não preenchida")]
+        [Trait("Categoria", "Jogos Tests")]
+        public void CriaDescricaoJogo_DescricaoNaoPreenchida_DeveLancarExcecao()
+        {
+            //Arrange
+            var novaDescricao = string.Empty;
+            //Act
+            var result = Assert.Throws<DomainException>(() => new Descricao(novaDescricao));
+            //Assert
+            Assert.Equal(MensagensDominio.JogoDescricaoObrigatoria, result.Message);
         }
     }
 }
