403282a481639e83ee871d2f86db8503a9bd6956
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-05-01 19:04:12 -0300

    adiciona interface IBibliotecaRepository

---
+++ src/FiapCloundGames.API/Infrastructure/Repository/IBibliotecaRepository.cs
@@ -0,0 +1,8 @@
+﻿using FiapCloundGames.API.Domain.Entities;
+
+namespace FiapCloundGames.API.Infrastructure.Repository
+{
+    public interface IBibliotecaRepository : IRepository<Biblioteca>
+    {
+    }
+}
