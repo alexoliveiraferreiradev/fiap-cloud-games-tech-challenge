6ffb53e8944932c545d9a7e34e66a0d493b3a532
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-05-01 14:23:07 -0300

    Adiciona ValueObject Periodo

---
+++ src/FiapCloundGames.API/Domain/ValueObjects/Periodo.cs
@@ -0,0 +1,30 @@
+﻿using FiapCloundGames.API.Domain.Common;
+using FiapCloundGames.API.Domain.Common.Exceptions;
+using FiapCloundGames.API.Domain.Resources;
+
+namespace FiapCloundGames.API.Domain.ValueObjects
+{
+    public class Periodo : ValueObject<Periodo>
+    {
+        public DateTime DataInicio { get;  }
+        public DateTime DataFim { get; set; }
+
+        public Periodo(DateTime dataFinal)
+        {
+            if (dataFinal <= DateTime.UtcNow) throw new DomainException(MensagensDominio.DataFimInvalida);
+            DataInicio = DateTime.UtcNow;
+            DataFim = dataFinal;
+        }
+        public Periodo() { }
+
+        protected override bool EqualsCore(Periodo other)
+        {
+            return DataInicio == other.DataInicio && DataFim == other.DataFim;  
+        }
+
+        protected override int GetHashCodeCore()
+        {
+            return DataInicio.GetHashCode() ^ DataFim.GetHashCode();
+        }
+    }
+}
