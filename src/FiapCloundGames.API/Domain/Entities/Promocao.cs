d7f56a08c28540c7ceaa2fcfb0e8dbb53b3b862d
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-05-01 14:31:12 -0300

    aplica ValueObject Periodo

--- src/FiapCloundGames.API/Domain/Entities/Promocao.cs
+++ src/FiapCloundGames.API/Domain/Entities/Promocao.cs
@@ -7,14 +7,13 @@ namespace FiapCloundGames.API.Domain.Entities
 {
     public class Promocao : EntityBase
     {
-        public Promocao(Guid jogoId, Preco valorPromocao, DateTime dataFim)
+        public Promocao(Guid jogoId, Preco valorPromocao, Periodo periodo)
         {
             JogoId = jogoId;
             ValorPromocao = valorPromocao;
             Ativo = true;
-            DataInicio = DateTime.UtcNow;
-            DataFim = dataFim;
-            DataCadastro = DateTime.UtcNow; 
+            Periodo = periodo;
+            DataCadastro = DateTime.UtcNow;
             ValidarEntidade();
         }
         protected Promocao() { }
@@ -22,28 +21,27 @@ namespace FiapCloundGames.API.Domain.Entities
         public Preco ValorPromocao { get; private set; }
         public bool Ativo { get; private set; }
         public DateTime DataCadastro { get; private set; }
-        public DateTime DataInicio { get; private set; }
-        public DateTime DataFim { get; private set; }
+        public Periodo Periodo { get; private set; }
         public DateTime DataAlteracao { get; private set; }
 
         protected override void ValidarEntidade()
         {
             if (JogoId == Guid.Empty) throw new DomainException(MensagensDominio.JogoNaoEncontrado);
-            if (DataFim <= DateTime.UtcNow) throw new DomainException(MensagensDominio.PromocaoDataFimInvalida);
+            AssertionConcern.AssertArgumentNotNull(Periodo, MensagensDominio.PeriodoObrigatorio);
         }
 
         public bool EstaValida() =>
-            Ativo && DateTime.UtcNow >= DataInicio && DateTime.UtcNow <= DataFim;
+            Ativo && DateTime.UtcNow >= Periodo.DataInicio && DateTime.UtcNow <= Periodo.DataFim;
 
         public void Desativar()
         {
             if (!EstaValida()) throw new DomainException(MensagensDominio.PromocaoInativa);
-             Ativo = false;
-            DataAlteracao = DateTime.UtcNow;    
+            Ativo = false;
+            DataAlteracao = DateTime.UtcNow;
         }
 
         public void AtualizarPrecoPromocional(Preco novoValor)
-        {            
+        {
             ValorPromocao = novoValor;
             DataAlteracao = DateTime.UtcNow;
         }
