b21dddb15b362cfedc942cbdd06d6a4084e7ebb3
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-05-01 21:34:38 -0300

    adiciona métodos Adicionar e AtualizarDados

--- src/FiapCloundGames.API/Domain/Entities/Biblioteca.cs
+++ src/FiapCloundGames.API/Domain/Entities/Biblioteca.cs
@@ -10,7 +10,7 @@ namespace FiapCloundGames.API.Domain.Entities
     {
         public Guid UsuarioId { get; private set; }
         public Guid JogoId { get; private set; }
-        public NomeJogo Nome { get;private set; }
+        public NomeJogo Nome { get; private set; }
         public Descricao Descricao { get; private set; }
         public GeneroJogo Genero { get; private set; }
         public DateTime DataCadastro { get; private set; }
@@ -18,7 +18,7 @@ namespace FiapCloundGames.API.Domain.Entities
         public bool Ativo { get; private set; }
 
         public Biblioteca(Guid usuarioId, Guid jogoId)
-        {            
+        {
             UsuarioId = usuarioId;
             JogoId = jogoId;
             DataCadastro = DateTime.UtcNow;
@@ -30,6 +30,32 @@ namespace FiapCloundGames.API.Domain.Entities
         {
             AssertionConcern.AssertArgumentNotNull(UsuarioId, MensagensDominio.UsuarioNaoEncontrado);
             AssertionConcern.AssertArgumentNotNull(JogoId, MensagensDominio.JogoNaoEncontrado);
+        }
+
+        public void Adicionar(NomeJogo nomeJogo, Descricao descricaoJogo, GeneroJogo generoJogo)
+        {
+            if (!Ativo) throw new DomainException(MensagensDominio.BibliotecaInativa);
+            AssertionConcern.AssertArgumentNotNull(nomeJogo, MensagensDominio.JogoNomeObrigatorio);
+            AssertionConcern.AssertArgumentNotNull(descricaoJogo, MensagensDominio.JogoDescricaoObrigatoria);
+            AssertionConcern.AssertArgumentNotNull(generoJogo, MensagensDominio.JogoGeneroObrigatorio);
+
+            Nome = nomeJogo;
+            Descricao = descricaoJogo;
+            Genero = generoJogo;
+        }
+        public void AtualizarDados(NomeJogo nomeJogo, Descricao descricaoJogo, GeneroJogo generoJogo)
+        {
+            if (!Ativo) throw new DomainException(MensagensDominio.BibliotecaInativa);
+            AssertionConcern.AssertArgumentNotNull(nomeJogo, MensagensDominio.JogoNomeObrigatorio);
+            AssertionConcern.AssertArgumentNotNull(descricaoJogo, MensagensDominio.JogoDescricaoObrigatoria);
+            AssertionConcern.AssertArgumentNotNull(generoJogo, MensagensDominio.JogoGeneroObrigatorio);
+
+            if (Nome != nomeJogo) Nome = nomeJogo;
+
+            if (Descricao != descricaoJogo) Descricao = descricaoJogo;
+
+            if (Genero != generoJogo) Genero = generoJogo;
+
         }
     }
 }
