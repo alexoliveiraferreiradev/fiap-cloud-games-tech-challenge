72102cc0882912bdf1a478858eb958842473c9cc
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-05-01 22:26:29 -0300

    adiciona teste AdicionaJogo_JogoValido_DeveAdicionarComSucesso

--- tests/FiapCloundGames.UnitTests/Application/Services/BibliotecaServiceTests.cs
+++ tests/FiapCloundGames.UnitTests/Application/Services/BibliotecaServiceTests.cs
@@ -11,23 +11,30 @@ namespace FiapCloundGames.UnitTests.Application.Services
     public class BibliotecaServiceTests
     {
         private readonly JogosFixture _jogoFixture;
+        private readonly UsuarioFixture _usuarioFixture;
         public BibliotecaServiceTests()
         {
             _jogoFixture = new JogosFixture();
+            _usuarioFixture = new UsuarioFixture();
         }
         [Fact(DisplayName ="Sucesso ao adicionar jogo na biblioteca - adiciona jogo com sucesso")]
         [Trait("Categoria","Biblioteca Services")]
         public async Task AdicionaJogo_JogoValido_DeveAdicionarComSucesso()
         {
             //Arrage
+            var usuario = _usuarioFixture.ObtemJogadorComSucesso();
             var jogo = _jogoFixture.ObtemJogosComSucesso();
             var criaJogoBibliotecaRequest = new CriaBibliotecaRequest(jogo.Nome.Valor, jogo.Descricao.Valor, jogo.Genero);
             //Mock
             var repoMock = new Mock<IBibliotecaRepository>();
-            var service = new BibliotecaService(repoMock.Object);
+            var usuarioMock = new Mock<IUsuarioRepository>();
+            var jogoMock = new Mock<IJogosRepository>();
+            var service = new BibliotecaService(repoMock.Object,usuarioMock.Object,jogoMock.Object);
+            usuarioMock.Setup(u => u.ObterPorId(usuario.Id)).ReturnsAsync(usuario);
+            jogoMock.Setup(j => j.ObterPorId(jogo.Id)).ReturnsAsync(jogo);
             //Act
-            await service.AdicionaJogo(criaJogoBibliotecaRequest);
-            //Assert
+            await service.AdicionaJogo(usuario.Id,jogo.Id);
+            //Assert            
             repoMock.Verify(r => r.Adicionar(It.IsAny<Biblioteca>()), Times.Once);
         }
     }
