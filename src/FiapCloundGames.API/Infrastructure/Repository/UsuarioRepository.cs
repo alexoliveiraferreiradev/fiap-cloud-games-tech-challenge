b8a7d34644093f316a64291de36c5524d8a9f079
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-05-02 19:20:50 -0300

    adiciona o UsuarioRepository

---
+++ src/FiapCloundGames.API/Infrastructure/Repository/UsuarioRepository.cs
@@ -0,0 +1,21 @@
+﻿using FiapCloundGames.API.Domain.Entities;
+using FiapCloundGames.API.Domain.Repositories;
+using FiapCloundGames.API.Infrastructure.Persistance.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace FiapCloundGames.API.Infrastructure.Repository
+{
+    public class UsuarioRepository : Repository<Usuario>, IUsuarioRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public UsuarioRepository(ApplicationDbContext dbContext) : base(dbContext)
+        {
+            _dbContext = dbContext; 
+        }
+
+        public async Task<Usuario?> ObterPorEmail(string email)
+        {
+            return await _dbContext.Usuarios.AsNoTracking().FirstOrDefaultAsync(p => p.EmailUsuario.Valor == email);
+        }
+    }
+}
