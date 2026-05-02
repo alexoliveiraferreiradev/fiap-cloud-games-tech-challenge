69ef29ad305b5c7e1305ad7d370616c3153dd685
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-05-02 14:31:51 -0300

    aplica DataAnnotation no record

--- src/FiapCloundGames.API/Application/Dtos/Jogos/CriarJogoRequest.cs
+++ src/FiapCloundGames.API/Application/Dtos/Jogos/CriarJogoRequest.cs
@@ -1,7 +1,18 @@
 ﻿using FiapCloundGames.API.Domain.Enum;
-using FiapCloundGames.API.Domain.ValueObjects;
+using FiapCloundGames.API.Domain.Resources;
+using System.ComponentModel.DataAnnotations;
 
 namespace FiapCloundGames.API.Application.Dtos.Jogos
 {
-    public record CriarJogoRequest(string Nome, string Descricao, decimal preco, GeneroJogo Genero);
+    public record CriarJogoRequest(
+        [Required(ErrorMessage = DataAnnotationMessage.ErroRequired)]
+        [StringLength(100,ErrorMessage = DataAnnotationMessage.ErroCaracteres,MinimumLength =2)]
+        string Nome,
+        [Required(ErrorMessage = DataAnnotationMessage.ErroRequired)]
+        [StringLength(500,ErrorMessage = DataAnnotationMessage.ErroCaracteres,MinimumLength =7)]
+        string Descricao,
+        [Required(ErrorMessage =DataAnnotationMessage.ErroRequired)]
+        decimal preco,
+        [Required(ErrorMessage =DataAnnotationMessage.ErroRequired)]
+        GeneroJogo Genero);
 }
