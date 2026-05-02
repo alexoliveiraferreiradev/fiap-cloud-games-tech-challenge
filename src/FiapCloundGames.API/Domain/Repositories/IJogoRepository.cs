9aaaad9d42361459eae882a1369134cfc4894a11
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-05-02 18:49:50 -0300

    aceita nulidade dos objetos

--- src/FiapCloundGames.API/Domain/Repositories/IJogoRepository.cs
+++ src/FiapCloundGames.API/Domain/Repositories/IJogoRepository.cs
@@ -4,9 +4,9 @@ namespace FiapCloundGames.API.Domain.Repositories
 {
     public interface IJogoRepository : IRepository<Jogo>
     {
-        Task<Jogo> ObtemPorNome(string nomeJogo);
+        Task<Jogo?> ObtemPorNome(string nomeJogo);
         Task<IEnumerable<Jogo>> ObtemJogosAtivos();
 
-        Task<Promocao> ObterPromocaoPorId(Guid id);
+        Task<Promocao?> ObterPromocaoPorId(Guid id);
     }
 }
