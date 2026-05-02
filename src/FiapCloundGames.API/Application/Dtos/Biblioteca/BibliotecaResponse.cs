b5ce5add753418f3633e9158125cc9af39ad841a
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-05-02 14:47:01 -0300

    passa o record para class

--- src/FiapCloundGames.API/Application/Dtos/Biblioteca/BibliotecaResponse.cs
+++ src/FiapCloundGames.API/Application/Dtos/Biblioteca/BibliotecaResponse.cs
@@ -1,10 +1,15 @@
 ﻿namespace FiapCloundGames.API.Application.Dtos.Biblioteca
 {
-    public record BibliotecaResponse(
-        Guid JogoId,
-        string NomeJogo,
-        string Descricao,
-        string Genero,
-        DateTime DataAquisicao
-        );
+    public class BibliotecaResponse
+    {
+        public Guid JogoId { get; set; }
+        public string NomeJogo { get; set; }
+        public string Descricao { get; set; }
+        public string Genero { get; set; }
+        public DateTime DataAquisicao { get; set; }
+
+        public BibliotecaResponse()
+        {            
+        }
+    }
 }
