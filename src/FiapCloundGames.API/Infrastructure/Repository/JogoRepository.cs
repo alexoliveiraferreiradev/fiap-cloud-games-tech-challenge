30f3d004008f25a7a9501003698fae741c0ce6ab
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-05-02 18:51:25 -0300

    adiciona repositórios

---
+++ src/FiapCloundGames.API/Infrastructure/Repository/JogoRepository.cs
@@ -0,0 +1,35 @@
+﻿using FiapCloundGames.API.Domain.Entities;
+using FiapCloundGames.API.Domain.Repositories;
+using FiapCloundGames.API.Infrastructure.Persistance.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace FiapCloundGames.API.Infrastructure.Repository
+{
+    public class JogoRepository : Repository<Jogo>, IJogoRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public JogoRepository(ApplicationDbContext dbContext) : base(dbContext)
+        {            
+            _dbContext = dbContext;
+        }
+
+        public async Task<IEnumerable<Jogo>> ObtemJogosAtivos()
+        {
+            return await _dbContext.Jogos.Where(x => x.Ativo == true).ToListAsync();
+        }
+
+        public async Task<Jogo?> ObtemPorNome(string nomeJogo)
+        {
+            return await _dbContext.Jogos.Where(x => x.Nome.Valor.Equals(nomeJogo)).FirstOrDefaultAsync();
+        }
+
+        public async Task<Promocao?> ObterPromocaoPorId(Guid id)
+        {
+            var jogosPorPromocao = await _dbContext.Jogos
+                .Include(j => j.Promocoes)
+                .FirstOrDefaultAsync(j=>j.Promocoes.Any(p=>p.Id == id));
+
+            return jogosPorPromocao?.Promocoes.FirstOrDefault(p => p.Id == id);
+        }
+    }
+}
