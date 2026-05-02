7fce50fd68f5ccb87ddd51bb2edd5e4543850e9a
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-05-02 15:15:16 -0300

    passa de record para classe

--- src/FiapCloundGames.API/Application/Dtos/Usuario/LoginRequest.cs
+++ src/FiapCloundGames.API/Application/Dtos/Usuario/LoginRequest.cs
@@ -1,4 +1,24 @@
-﻿namespace FiapCloundGames.API.Application.Dtos.Usuario
+﻿using FiapCloundGames.API.Domain.Resources;
+using System.ComponentModel.DataAnnotations;
+
+namespace FiapCloundGames.API.Application.Dtos.Usuario
 {
-    public record LoginRequest(string emailUsuario, string senhaUsuario);
+    public class LoginRequest {
+        [Required(ErrorMessage = DataAnnotationMessage.ErroRequired)]
+        [EmailAddress(ErrorMessage = DataAnnotationMessage.ErroFormato)]
+        [StringLength(100, ErrorMessage = DataAnnotationMessage.ErroCaracteres, MinimumLength = 7)]
+        public string Email { get; set; }
+        [Required(ErrorMessage = DataAnnotationMessage.ErroRequired)]
+        [DataType(DataType.Password)]
+        [StringLength(60, ErrorMessage = DataAnnotationMessage.ErroCaracteres, MinimumLength = 8)]
+        public string Senha { get; set; }
+        public LoginRequest()
+        {            
+        }
+        public LoginRequest(string emailUsuario, string senhaUsuario)
+        {
+            Email = emailUsuario;
+            Senha = senhaUsuario;
+        }
+    }
 }
