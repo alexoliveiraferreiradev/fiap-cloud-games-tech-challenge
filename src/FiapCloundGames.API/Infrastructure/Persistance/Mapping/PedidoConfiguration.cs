fd1e53c7128a9f919fd38074dc91632ae7f08324
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-05-02 01:51:05 -0300

    adiciona configuracao de Pedido

---
+++ src/FiapCloundGames.API/Infrastructure/Persistance/Mapping/PedidoConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using FiapCloundGames.API.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FiapCloundGames.API.Infrastructure.Persistance.Mapping
+{
+    public class PedidoConfiguration : IEntityTypeConfiguration<Pedido>
+    {
+        public void Configure(EntityTypeBuilder<Pedido> builder)
+        {
+            builder.ToTable("Pedidos");
+            builder.HasKey(p => p.Id);
+
+            builder.OwnsOne(p => p.ValorTotal, v =>
+            {
+                v.Property(p => p.Valor).HasColumnName("PrecoTotal").HasDefaultValue(0).HasPrecision(18, 2);
+            });
+
+            builder.HasOne(p => p.Usuario)
+               .WithMany()            
+               .HasForeignKey(p => p.UsuarioId)
+               .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
