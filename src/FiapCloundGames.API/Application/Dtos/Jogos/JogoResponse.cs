19e3e6eabadc2a62050b6febd25fe12f84a5d73f
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-05-02 00:01:12 -0300

    adiciona record JogoResponse

--- src/FiapCloundGames.API/Application/Dtos/Jogos/JogoResponse.cs
+++ src/FiapCloundGames.API/Application/Dtos/Jogos/JogoResponse.cs
@@ -1,12 +1,14 @@
 ﻿namespace FiapCloundGames.API.Application.Dtos.Jogos
 {
-    public class JogoResponse
+    public record JogoResponse
+    (
+        Guid Id,
+        string Nome,
+        string Descricao,
+        decimal PrecoOriginal,
+        decimal PrecoAtual
+    )
     {
-        public Guid Id { get; set; }
-        public string Nome { get; set; }
-        public string Descricao { get; set; }
-        public decimal PrecoOriginal { get; set; }
-        public decimal PrecoAtual { get; set; }
         public bool TemDesconto => PrecoAtual < PrecoOriginal;
     }
 }
