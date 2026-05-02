cbc1c78ce608118ac6a761220562b3319130b0ad
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-05-02 14:33:19 -0300

    adiciona DataAnnotation ao record

--- src/FiapCloundGames.API/Application/Dtos/Jogos/UpdateJogosRequest.cs
+++ src/FiapCloundGames.API/Application/Dtos/Jogos/UpdateJogosRequest.cs
@@ -1,7 +1,19 @@
 ﻿using FiapCloundGames.API.Domain.Enum;
+using FiapCloundGames.API.Domain.Resources;
 using FiapCloundGames.API.Domain.ValueObjects;
+using System.ComponentModel.DataAnnotations;
 
 namespace FiapCloundGames.API.Application.Dtos.Jogos
 {
-    public record UpdateJogosRequest(string novoNome, string novaDescricao, decimal novoPreco, GeneroJogo novoGenero);
+    public record UpdateJogosRequest(
+        [Required(ErrorMessage = DataAnnotationMessage.ErroRequired)]
+        [StringLength(100,ErrorMessage = DataAnnotationMessage.ErroCaracteres,MinimumLength =2)]
+        string novoNome,
+        [Required(ErrorMessage = DataAnnotationMessage.ErroRequired)]
+        [StringLength(500,ErrorMessage = DataAnnotationMessage.ErroCaracteres,MinimumLength =7)]
+        string novaDescricao,
+        [Required(ErrorMessage =DataAnnotationMessage.ErroRequired)]
+        decimal novoPreco,
+        [Required(ErrorMessage =DataAnnotationMessage.ErroRequired)]
+        GeneroJogo novoGenero);
 }
