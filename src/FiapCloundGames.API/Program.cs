fc7f4ba99a8b0446368553074faeac0223993dfe
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-05-02 15:47:25 -0300

    adiciona connection string e adiciona o ApplicationDbContext

--- src/FiapCloundGames.API/Program.cs
+++ src/FiapCloundGames.API/Program.cs
@@ -1,10 +1,20 @@
+using FiapCloundGames.API.Infrastructure.Persistance.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
 var app = builder.Build();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(connectionString);
+});
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
