80978050a0b222ec5db7b8143ab39597e60f5dc4
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-05-02 13:08:12 -0300

    torna PedidoJogo como uma entidade e muda o ValidarEntidade

--- src/FiapCloundGames.API/Domain/Entities/PedidoJogo.cs
+++ src/FiapCloundGames.API/Domain/Entities/PedidoJogo.cs
@@ -4,7 +4,7 @@ using FiapCloundGames.API.Domain.ValueObjects;
 
 namespace FiapCloundGames.API.Domain.Entities
 {
-    public class PedidoJogo
+    public class PedidoJogo : EntityBase   
     {
         public Guid JogoId { get; private set; }
         public Preco PrecoNoMomento { get; private set; }
@@ -15,15 +15,15 @@ namespace FiapCloundGames.API.Domain.Entities
 
         public PedidoJogo(Guid jogoId, Preco precoNoMomento)
         {
-            ValidaEntidades(jogoId, precoNoMomento);
             JogoId = jogoId;
             PrecoNoMomento = precoNoMomento;
+            ValidarEntidade();
         }
 
-        private void ValidaEntidades(Guid jogoId,Preco preco)
+        protected override void ValidarEntidade()
         {
-            AssertionConcern.AssertArgumentNotNull(jogoId, MensagensDominio.JogoNaoEncontrado);
-            AssertionConcern.AssertArgumentNotNull(preco, MensagensDominio.PrecoObrigatorio);
+            AssertionConcern.AssertArgumentNotNull(JogoId, MensagensDominio.JogoNaoEncontrado);
+            AssertionConcern.AssertArgumentNotNull(PrecoNoMomento, MensagensDominio.PrecoObrigatorio);
         }
     }
 }
