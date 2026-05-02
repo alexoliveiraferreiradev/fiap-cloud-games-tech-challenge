b613930d13fe1738464008bc47d0e113e5c5ffa4
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-05-01 21:57:25 -0300

    cria dto UpdateBibliotecaRequest

---
+++ src/FiapCloundGames.API/Application/Dtos/Biblioteca/UpdateBibliotecaRequest.cs
@@ -0,0 +1,6 @@
+﻿using FiapCloundGames.API.Domain.Enum;
+
+namespace FiapCloundGames.API.Application.Dtos.Biblioteca
+{
+    public record UpdateBibliotecaRequest(string nomeJogo, string descricaoJogo, GeneroJogo generoJogo);
+}
