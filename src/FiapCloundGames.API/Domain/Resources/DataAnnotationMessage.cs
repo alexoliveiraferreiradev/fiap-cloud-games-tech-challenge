9632b45d3246623791e82db989848661a04e5cea
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-05-02 14:31:11 -0300

    adiciona classe Attribute de DataAnnotationMessage

---
+++ src/FiapCloundGames.API/Domain/Resources/DataAnnotationMessage.cs
@@ -0,0 +1,11 @@
+﻿namespace FiapCloundGames.API.Domain.Resources
+{
+    public class DataAnnotationMessage : Attribute
+    {
+        public const string ErroCaracteres = "O campo {0} precisa ter entre {2} e {1} caracteres";
+
+        public const string ErroRequired = "O campo {0} é obrigatório";
+
+        public const string ErroFormatoEmail = "O campo {0} está em formato incorreto";
+    }
+}
