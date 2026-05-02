f9963b49faff183c7afd50fa008c3bae613f600f
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-05-01 21:41:10 -0300

    adiciona interface de Service IBibliotecaService com métodos : Adiciona, AtualizaDados e Limpar

---
+++ src/FiapCloundGames.API/Application/Services/Interfaces/IBibliotecaService.cs
@@ -0,0 +1,12 @@
+﻿using FiapCloundGames.API.Domain.Enum;
+using FiapCloundGames.API.Domain.ValueObjects;
+
+namespace FiapCloundGames.API.Application.Services.Interfaces
+{
+    public interface IBibliotecaService
+    {
+        Task Adiciona(NomeJogo nomeJogo, Descricao descricao, GeneroJogo generoJogo);
+        Task AtualizaDados(NomeJogo nomeJogo, Descricao descricao, GeneroJogo generoJogo);
+        Task Limpar();
+    }
+}
