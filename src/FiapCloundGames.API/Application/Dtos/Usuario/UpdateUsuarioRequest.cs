d6358640a6087cfda12d13fd25fee16d37178a55
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-05-02 15:17:28 -0300

    altera o record para classe

--- src/FiapCloundGames.API/Application/Dtos/Usuario/UpdateUsuarioRequest.cs
+++ src/FiapCloundGames.API/Application/Dtos/Usuario/UpdateUsuarioRequest.cs
@@ -1,4 +1,25 @@
-﻿namespace FiapCloundGames.API.Application.Dtos.Usuario
+﻿using FiapCloundGames.API.Domain.Resources;
+using System.ComponentModel.DataAnnotations;
+
+namespace FiapCloundGames.API.Application.Dtos.Usuario
 {
-    public record UpdateUsuarioRequest(string nomeUsuario, string emailUsuario, string senhaUsuario,string reSenhaUsuario);
+    public class UpdateUsuarioRequest
+    {
+        [Required(ErrorMessage = DataAnnotationMessage.ErroRequired)]
+        [StringLength(40, ErrorMessage = DataAnnotationMessage.ErroCaracteres, MinimumLength = 3)]
+        public string NomeUsuario { get; set; }
+        [Required(ErrorMessage = DataAnnotationMessage.ErroRequired)]
+        [EmailAddress(ErrorMessage = DataAnnotationMessage.ErroFormato)]
+        [StringLength(100, ErrorMessage = DataAnnotationMessage.ErroCaracteres, MinimumLength = 7)]
+        public string EmailUsuario { get; set; }
+        [Required(ErrorMessage = DataAnnotationMessage.ErroRequired)]
+        [DataType(DataType.Password, ErrorMessage = DataAnnotationMessage.ErroFormato)]
+        [StringLength(60, ErrorMessage = DataAnnotationMessage.ErroCaracteres, MinimumLength = 8)]
+        public string SenhaUsuario { get; set; }
+        [Required(ErrorMessage = DataAnnotationMessage.ErroRequired)]
+        [DataType(DataType.Password)]
+        [Compare("Senha", ErrorMessage = DataAnnotationMessage.ErroConfirmacaoSenha)]
+        [StringLength(60, ErrorMessage = DataAnnotationMessage.ErroCaracteres, MinimumLength = 8)]
+        public string ConfirmacaoSenha { get; set; }
+    }
 }
