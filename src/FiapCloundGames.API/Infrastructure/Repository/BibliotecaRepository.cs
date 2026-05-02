4759364c6c5f88d2fe8d12ec6ed98fe70d8dcdf2
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-05-02 19:07:41 -0300

    adiciona BibliotecaRepository

---
+++ src/FiapCloundGames.API/Infrastructure/Repository/BibliotecaRepository.cs
@@ -0,0 +1,35 @@
+﻿using FiapCloundGames.API.Application.Dtos.Biblioteca;
+using FiapCloundGames.API.Domain.Entities;
+using FiapCloundGames.API.Domain.Repositories;
+using FiapCloundGames.API.Infrastructure.Persistance.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace FiapCloundGames.API.Infrastructure.Repository
+{
+    public class BibliotecaRepository : Repository<Biblioteca>, IBibliotecaRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public BibliotecaRepository(ApplicationDbContext dbContext) : base(dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task<IEnumerable<BibliotecaResponse>> ObterJogosPorUsuario(Guid usuarioId)
+        {
+            return await _dbContext.Bibliotecas
+                     .AsNoTracking() 
+                    .Where(b => b.UsuarioId == usuarioId)
+                    .Select(b => new BibliotecaResponse
+                    {
+                        JogoId = b.JogoId,
+                        Descricao = b.Jogo.Descricao.Valor,
+                        DataAquisicao = b.DataCadastro,
+                        Genero = b.Jogo.Genero.ToString()
+                    }).ToListAsync();
+        }
+
+        public async Task<bool> VerificaSeUsuarioPossuiJogo(Guid usuarioId, Guid jogoId)
+        {
+            return await _dbContext.Bibliotecas.AnyAsync(p =>p.UsuarioId == usuarioId && p.JogoId == jogoId);
+        }
+    }
+}
