930b192f0a1465e91f44b5ddc2fccd5ec3bcbe96
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-05-02 19:14:19 -0300

    adiciona o PedidoRepository

---
+++ src/FiapCloundGames.API/Infrastructure/Repository/PedidoRepository.cs
@@ -0,0 +1,26 @@
+﻿using FiapCloundGames.API.Domain.Entities;
+using FiapCloundGames.API.Domain.Repositories;
+using FiapCloundGames.API.Infrastructure.Persistance.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace FiapCloundGames.API.Infrastructure.Repository
+{
+    public class PedidoRepository : Repository<Pedido>, IPedidoRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public PedidoRepository(ApplicationDbContext dbContext) : base(dbContext)
+        {
+            _dbContext = dbContext; 
+        }
+
+        public async Task<IEnumerable<Pedido>> ObtemHistoricoPorUsuario(Guid usuarioId)
+        {
+            return await _dbContext.Pedidos
+                 .AsNoTracking()
+                 .Include(p=>p.Jogos)
+                 .Where(x => x.UsuarioId == usuarioId)
+                 .OrderByDescending(x=>x.DataCadastro)
+                 .ToListAsync();
+        }
+    }
+}
