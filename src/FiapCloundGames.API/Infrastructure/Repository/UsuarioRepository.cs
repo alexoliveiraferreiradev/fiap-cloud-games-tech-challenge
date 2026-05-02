36c1e2b251a76e5c06d8f8e88d5d4e13c35850c9
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-05-02 19:45:36 -0300

    deixa ToLower as comparações

--- src/FiapCloundGames.API/Infrastructure/Repository/UsuarioRepository.cs
+++ src/FiapCloundGames.API/Infrastructure/Repository/UsuarioRepository.cs
@@ -15,7 +15,17 @@ namespace FiapCloundGames.API.Infrastructure.Repository
 
         public async Task<Usuario?> ObterPorEmail(string email)
         {
-            return await _dbContext.Usuarios.AsNoTracking().FirstOrDefaultAsync(p => p.EmailUsuario.Valor == email);
+            return await _dbContext.Usuarios.AsNoTracking().FirstOrDefaultAsync(p => p.EmailUsuario.Valor.ToLower() == email.ToLower());
+        }
+
+        public async Task<bool> VerificaEmailCadastrado(string emailCadastrado)
+        {
+            return await _dbContext.Usuarios.AnyAsync(x => x.EmailUsuario.Valor.ToLower() == emailCadastrado.ToLower());
+        }
+
+        public async Task<bool> VerificaNomeCadastrado(string nomeCadastrado)
+        {
+            return await _dbContext.Usuarios.AnyAsync(x => x.NomeUsuario.Valor.ToLower() == nomeCadastrado.ToLower());
         }
     }
 }
