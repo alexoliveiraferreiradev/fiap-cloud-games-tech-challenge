e8d60ff6cb08b82552a2c87e341627abc5af46b8
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-05-02 00:02:23 -0300

    adiciona o método ObterJogosPorUsuario

--- src/FiapCloundGames.API/Infrastructure/Repository/IBibliotecaRepository.cs
+++ src/FiapCloundGames.API/Infrastructure/Repository/IBibliotecaRepository.cs
@@ -1,9 +1,11 @@
-﻿using FiapCloundGames.API.Domain.Entities;
+﻿using FiapCloundGames.API.Application.Dtos.Biblioteca;
+using FiapCloundGames.API.Domain.Entities;
 
 namespace FiapCloundGames.API.Infrastructure.Repository
 {
     public interface IBibliotecaRepository : IRepository<Biblioteca>
     {
         Task<bool> VerificaSeUsuarioPossuiJogo(Guid usuarioId, Guid jogoId);
+        Task<IEnumerable<BibliotecaResponse>> ObterJogosPorUsuario(Guid usuarioId);
     }
 }
