17aad9a6f9f116196489a8deffc33455eb6c158a
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-05-01 21:46:04 -0300

    adiciona BibliotecaService

---
+++ src/FiapCloundGames.API/Application/Services/Interfaces/BibliotecaService.cs
@@ -0,0 +1,24 @@
+﻿using FiapCloundGames.API.Domain.Enum;
+using FiapCloundGames.API.Domain.ValueObjects;
+using FiapCloundGames.API.Infrastructure.Repository;
+
+namespace FiapCloundGames.API.Application.Services.Interfaces
+{
+    public class BibliotecaService : IBibliotecaService
+    {
+        private readonly IBibliotecaRepository _bibliotecaRepository;
+        public BibliotecaService(IBibliotecaRepository bibliotecaRepository)
+        {
+            _bibliotecaRepository = bibliotecaRepository;   
+        }
+        public Task AdicionaJogo(NomeJogo nomeJogo, Descricao descricao, GeneroJogo generoJogo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task AtualizarDados(NomeJogo nomeJogo, Descricao descricao, GeneroJogo generoJogo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
