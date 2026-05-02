248838a3674455302582648cfc8c06b4800f7b36
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-05-01 21:48:39 -0300

    adiciona teste AdicionaJogo_JogoValido_DeveAdicionarComSucesso

---
+++ tests/FiapCloundGames.UnitTests/Application/Services/BibliotecaServiceTests.cs
@@ -0,0 +1,14 @@
+﻿namespace FiapCloundGames.UnitTests.Application.Services
+{
+    public class BibliotecaServiceTests
+    {
+        [Fact(DisplayName ="Sucesso ao adicionar biblioteca - adiciona com sucesso")]
+        [Trait("Categoria","Biblioteca Services")]
+        public void AdicionaJogo_JogoValido_DeveAdicionarComSucesso()
+        {
+            //Arrage
+            //Act
+            //Assert
+        }
+    }
+}
