bd7e560fae72efc0fd907cf1143419d46428ec76
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-05-01 22:42:39 -0300

    exclui dto UpdateBibliotecaRequest

--- src/FiapCloundGames.API/Application/Dtos/Biblioteca/UpdateBibliotecaRequest.cs
+++
@@ -1,6 +0,0 @@
-﻿using FiapCloundGames.API.Domain.Enum;
-
-namespace FiapCloundGames.API.Application.Dtos.Biblioteca
-{
-    public record UpdateBibliotecaRequest(string nomeJogo, string descricaoJogo, GeneroJogo generoJogo);
-}
