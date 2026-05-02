38b5684b121b12645e322ff00072fdcb925d2e33
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-05-02 14:48:17 -0300

    passa o record para class

--- src/FiapCloundGames.API/Application/Dtos/Jogos/CriarJogoRequest.cs
+++ src/FiapCloundGames.API/Application/Dtos/Jogos/CriarJogoRequest.cs
@@ -4,15 +4,17 @@ using System.ComponentModel.DataAnnotations;
 
 namespace FiapCloundGames.API.Application.Dtos.Jogos
 {
-    public record CriarJogoRequest(
+    public class CriarJogoRequest
+    {
         [Required(ErrorMessage = DataAnnotationMessage.ErroRequired)]
-        [StringLength(100,ErrorMessage = DataAnnotationMessage.ErroCaracteres,MinimumLength =2)]
-        string Nome,
+        [StringLength(100, ErrorMessage = DataAnnotationMessage.ErroCaracteres, MinimumLength = 2)]
+        public string Nome { get; set; }
         [Required(ErrorMessage = DataAnnotationMessage.ErroRequired)]
-        [StringLength(500,ErrorMessage = DataAnnotationMessage.ErroCaracteres,MinimumLength =7)]
-        string Descricao,
-        [Required(ErrorMessage =DataAnnotationMessage.ErroRequired)]
-        decimal preco,
-        [Required(ErrorMessage =DataAnnotationMessage.ErroRequired)]
-        GeneroJogo Genero);
+        [StringLength(500, ErrorMessage = DataAnnotationMessage.ErroCaracteres, MinimumLength = 7)]
+        public string Descricao { get; set; }
+        [Required(ErrorMessage = DataAnnotationMessage.ErroRequired)]
+        public decimal preco { get; set; }
+        [Required(ErrorMessage = DataAnnotationMessage.ErroRequired)]
+        public GeneroJogo Genero { get; set; }
+    }
 }
