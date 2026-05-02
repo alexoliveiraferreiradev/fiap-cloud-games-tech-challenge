c0771de3bf7f5483fc7e9e2f74d7987f7a587fd2
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-05-01 22:27:26 -0300

    adiciona referência IJogosRepository e IUsuarioRepository e altera o método de adicionarjogo

--- src/FiapCloundGames.API/Application/Services/Interfaces/BibliotecaService.cs
+++ src/FiapCloundGames.API/Application/Services/Interfaces/BibliotecaService.cs
@@ -1,5 +1,8 @@
 ﻿using FiapCloundGames.API.Application.Dtos.Biblioteca;
+using FiapCloundGames.API.Domain.Common.Exceptions;
+using FiapCloundGames.API.Domain.Entities;
 using FiapCloundGames.API.Domain.Enum;
+using FiapCloundGames.API.Domain.Resources;
 using FiapCloundGames.API.Domain.ValueObjects;
 using FiapCloundGames.API.Infrastructure.Repository;
 
@@ -8,13 +11,29 @@ namespace FiapCloundGames.API.Application.Services.Interfaces
     public class BibliotecaService : IBibliotecaService
     {
         private readonly IBibliotecaRepository _bibliotecaRepository;
-        public BibliotecaService(IBibliotecaRepository bibliotecaRepository)
+        private readonly IUsuarioRepository _usuarioRepository;
+        private readonly IJogosRepository _jogoRepository;
+        public BibliotecaService(IBibliotecaRepository bibliotecaRepository, IUsuarioRepository usuarioRepository,
+            IJogosRepository jogosRepository)
         {
             _bibliotecaRepository = bibliotecaRepository;   
+            _usuarioRepository = usuarioRepository; 
+            _jogoRepository = jogosRepository;  
         }
-        public Task AdicionaJogo(CriaBibliotecaRequest criaBibliotecaRequest)
+        public async Task AdicionaJogo(Guid usuarioId, Guid jogoId)
         {
-            throw new NotImplementedException();
+            var usuario = await _usuarioRepository.ObterPorId(usuarioId);
+
+            if (usuario == null) throw new DomainException(MensagensDominio.JogoNaoEncontrado);
+            if (!usuario.Ativo) throw new DomainException(MensagensDominio.UsuarioInativo);
+
+            var jogo = await _jogoRepository.ObterPorId(jogoId);
+            if (jogo == null) throw new DomainException(MensagensDominio.JogoNaoEncontrado);
+            if (!jogo.Ativo) throw new DomainException(MensagensDominio.JogoInvalido);
+
+            var biblioteca = new Biblioteca(usuarioId, jogoId);
+            biblioteca.AdicionaJogo(jogo.Nome, jogo.Descricao, jogo.Genero);
+            await _bibliotecaRepository.Adicionar(biblioteca);
         }
 
         public Task AtualizarDados(UpdateBibliotecaRequest updateBibliotecaRequest)
