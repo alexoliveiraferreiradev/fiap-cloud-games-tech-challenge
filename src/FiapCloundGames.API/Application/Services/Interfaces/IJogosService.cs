59330cf1ceafb268a4fbfad66c1c6ef4e4bd68e9
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-05-03 01:11:31 -0300

    altera os parâmetros de entrada dos services de crud

--- src/FiapCloundGames.API/Application/Services/Interfaces/IJogosService.cs
+++ src/FiapCloundGames.API/Application/Services/Interfaces/IJogosService.cs
@@ -6,13 +6,13 @@ namespace FiapCloundGames.API.Application.Services.Interfaces
 {
     public interface IJogosService
     {
-        Task<Jogo> AdicionaJogo(CriarJogoRequest request);
-        Task AtualizarJogo(Guid usuarioId,UpdateJogoRequest updateJogoRequest);
+        Task<Jogo> AdicionaJogo(Jogo jogo);
+        Task AtualizarJogo(Guid usuarioId,Jogo novoJogo);
         Task Desativar(Guid jogoId);
         Task Reativar(Guid jogoId);
         Task<bool> VerificaDuplicidadeNome(string nomeJogo);
-        Task AdicionarPromocao(CriaPromocaoRequest criaPromocaoRequest);
-        Task AtualizaPromocao(Guid promocaoId,UpdatePromocaoRequest criaPromocaoRequest);
+        Task AdicionarPromocao(Promocao promocao);
+        Task AtualizaPromocao(Guid promocaoId,Promocao novaPromocao);
         Task DesativarPromocao(Guid jogoId,Guid promocaoId);
         Task<IEnumerable<JogoResponse>> ObtemCatalagoJogos();
         Task<Jogo> ObtemJogoPorId(Guid jogoId);
