df62411c432f3bc9bb4840cfbcf9cd70097966bd
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-05-03 16:48:15 -0300

    adiciona o response de usuario

---
+++ src/FiapCloundGames.API/Application/Dtos/Usuario/UsuarioResponse.cs
@@ -0,0 +1,15 @@
+﻿using FiapCloundGames.API.Domain.Enum;
+
+namespace FiapCloundGames.API.Application.Dtos.Usuario
+{
+    public class UsuarioResponse
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public bool Ativo { get; set; }
+        public TipoUsuario PerfilUsuario {  get; set; }
+        public DateTime DataAlteracao { get; set; }
+        public MotivoExclusao? MotivoDesativacao { get; private set; }
+    }
+}
