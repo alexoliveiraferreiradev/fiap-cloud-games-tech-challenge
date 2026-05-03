56c347d178901bbe0746d172fd1f568d026079a4
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-05-02 22:27:11 -0300

    faz o HasData de acordo com o VO de usuario

--- src/FiapCloundGames.API/Infrastructure/Persistance/Mapping/UsuarioConfiguration.cs
+++ src/FiapCloundGames.API/Infrastructure/Persistance/Mapping/UsuarioConfiguration.cs
@@ -1,5 +1,6 @@
 ﻿using FiapCloundGames.API.Domain.Entities;
 using FiapCloundGames.API.Domain.Enum;
+using FiapCloundGames.API.Domain.ValueObjects;
 using FiapCloundGames.API.Infrastructure.Security;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
@@ -17,65 +18,37 @@ namespace FiapCloundGames.API.Infrastructure.Persistance.Mapping
         {
             builder.ToTable("Usuarios");
             builder.HasKey(p => p.Id);
-            builder.OwnsOne(u => u.NomeUsuario, n =>
-            {
-                n.Property(p => p.Valor).HasColumnName("Nome").IsRequired().HasMaxLength(50);
-            });
-
-            builder.OwnsOne(u => u.EmailUsuario, e =>
-            {
-                e.Property(p => p.Valor).HasColumnName("Email").IsRequired().HasMaxLength(100);
-            });
-
-            builder.OwnsOne(u => u.Senha, s =>
-            {
-                s.Property(p => p.Hash).HasColumnName("Senha").IsRequired().HasMaxLength(60);
-            });
-
-            builder.Property(u => u.MotivoDesativacao)
-                .IsRequired(false);            
 
             var adminId = Guid.Parse("AEA0B4F3-D220-4C8D-ABA8-D868BE7CA593");
-            var senhaHash = _passwordHasher.HashPassword("SenhaAdmin@123");
             builder.HasData(new
             {
                 Id = adminId,
                 Ativo = true,
                 Perfil = TipoUsuario.Administrador,
-                Nome_Valor = "Admin Sistema",
-                Email_Valor = "admin@fiapcloundgames.com.br",
-                Senha_Hash = senhaHash,
                 DataCadastro = new DateTime(2026, 5, 2),
                 DataAlteracao = new DateTime(2026, 5, 2)
             });
 
-            var jogadorId = Guid.Parse("E99C8CAF-7067-49B4-8EEF-B0D7ED801033");
-            senhaHash = _passwordHasher.HashPassword("SenhaJogador@123");
-            builder.HasData(new
+            builder.OwnsOne(u => u.NomeUsuario, n =>
             {
-                Id = jogadorId,
-                Ativo = true,
-                Perfil = TipoUsuario.Jogador,
-                Nome_Valor = "Jogador 1",
-                Email_Valor = "jogador1@fiapcloundgames.com.br",
-                Senha_Hash = senhaHash,
-                DataCadastro = new DateTime(2026, 5, 2),
-                DataAlteracao = new DateTime(2026, 5, 2)
+                n.Property(p => p.Valor).HasColumnName("Nome").IsRequired().HasMaxLength(50);
+                n.HasData(new { UsuarioId = adminId, Valor = "Administrador Sistema" });
             });
 
-            jogadorId = Guid.Parse("BC359373-699B-4152-B63C-4FBF5A509AAD");
-            senhaHash = _passwordHasher.HashPassword("SenhaJogador@1234");
-            builder.HasData(new
+            builder.OwnsOne(u => u.EmailUsuario, e =>
             {
-                Id = jogadorId,
-                Ativo = true,
-                Perfil = TipoUsuario.Jogador,
-                Nome_Valor = "Jogador 2",
-                Email_Valor = "jogador2@fiapcloundgames.com.br",
-                Senha_Hash = senhaHash,
-                DataCadastro = new DateTime(2026, 5, 2),
-                DataAlteracao = new DateTime(2026, 5, 2)
+                e.Property(p => p.Valor).HasColumnName("Email").IsRequired().HasMaxLength(100);
+                e.HasData(new { UsuarioId = adminId, Valor = "admin@fiapcloudgames.com.br" });
             });
+
+            builder.OwnsOne(u => u.Senha, s =>
+            {
+                s.Property(p => p.Hash).HasColumnName("Senha").IsRequired().HasMaxLength(60);
+                s.HasData(new { UsuarioId = adminId, Hash = "$2a$11$Soy4TsNUDtuazT6CJulPleFnp82cF5BkICiOmF9sk19x0X6pMAic." });
+            });
+
+            builder.Property(u => u.MotivoDesativacao)
+                .IsRequired(false);
         }
     }
 }
