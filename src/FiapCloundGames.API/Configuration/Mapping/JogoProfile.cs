df729f806745d276f3dedd6bbf673134afafa6a3
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-05-03 16:47:52 -0300

    adiciona o mapper de Jogo

---
+++ src/FiapCloundGames.API/Configuration/Mapping/JogoProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using FiapCloundGames.API.Application.Dtos.Jogos;
+using FiapCloundGames.API.Domain.Entities;
+
+namespace FiapCloundGames.API.Configuration.Mapping
+{
+    public class JogoProfile : Profile
+    {
+        public JogoProfile()
+        {
+            CreateMap<Jogo, JogoResponse>()
+                .ForMember(dest => dest.Nome, opt => opt.MapFrom(src => src.Nome.Valor))
+                .ForMember(dest => dest.Descricao, opt => opt.MapFrom(src => src.Descricao.Valor))
+                .ForMember(dest => dest.PrecoOriginal, opt => opt.MapFrom(src => src.PrecoBase.Valor))
+                .ForMember(dest=>dest.PrecoAtual,opt=>opt.MapFrom(src=>src.ObterPrecoAtual().Valor));
+
+        }
+    }
+}
