2cc11c70305083517547265c14e2250a657266ba
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-05-03 02:37:39 -0300

    adiciona middleware de GlobalExceptionHandler

---
+++ src/FiapCloundGames.API/Middlewares/GlobalExceptionHandler.cs
@@ -0,0 +1,36 @@
+﻿using FiapCloundGames.API.Application.Common.Exceptions;
+using FiapCloundGames.API.Domain.Common.Exceptions;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FiapCloundGames.API.Middlewares
+{
+    public class GlobalExceptionHandler : IExceptionHandler
+    {
+        private readonly ILogger<GlobalExceptionHandler> _logger;
+        public GlobalExceptionHandler(ILogger<GlobalExceptionHandler> logger)
+        {
+            _logger = logger;   
+        }
+        public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, 
+            Exception exception, 
+            CancellationToken cancellationToken)
+        {
+            _logger.LogError(exception, "Ocorreu um erro: {Message}", exception.Message);
+            if (exception is DomainException || exception is BusinessException)
+            {
+                var problemDetails = new ProblemDetails
+                {
+                    Status = StatusCodes.Status400BadRequest,
+                    Title = "Erro de Negócio",
+                    Detail = exception.Message 
+                };
+
+                httpContext.Response.StatusCode = problemDetails.Status.Value;
+                await httpContext.Response.WriteAsJsonAsync(problemDetails, cancellationToken);
+                return true;
+            }
+            return false;
+        }
+    }
+}
