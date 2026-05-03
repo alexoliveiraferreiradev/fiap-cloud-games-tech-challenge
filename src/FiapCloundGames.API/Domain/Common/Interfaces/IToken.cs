57804b20c9572eb50caa976abab6cf0cf272f6ff
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-05-03 12:26:15 -0300

    cria a interface de IToken

---
+++ src/FiapCloundGames.API/Domain/Common/Interfaces/IToken.cs
@@ -0,0 +1,13 @@
+﻿using System.Security.Claims;
+
+namespace FiapCloundGames.API.Domain.Common.Interfaces
+{
+    public interface IToken
+    {
+        Task<string> GenerateToken(string configurationTagName, string emailUsuario, IEnumerable<Claim> claims);
+
+        bool ValidateToken(string token);   
+
+        string TokenGenerate(IEnumerable<Claim> claims, string configurationTagName);
+    }
+}
