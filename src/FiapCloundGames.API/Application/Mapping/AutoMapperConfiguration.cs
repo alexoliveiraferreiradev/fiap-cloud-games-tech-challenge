7e05054e457ff4f09abb650105a5c6647a88f0e3
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-05-03 00:54:24 -0300

    adiciona o AutoMapper ao projeto e cria sua configuração

---
+++ src/FiapCloundGames.API/Application/Mapping/AutoMapperConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using FiapCloundGames.API.Application.Dtos.Jogos;
+using FiapCloundGames.API.Domain.Entities;
+
+namespace FiapCloundGames.API.Application.Mapping
+{
+    public class AutoMapperConfiguration : Profile
+    {
+        public AutoMapperConfiguration()
+        {
+            CreateMap<Jogo, JogoResponse>()
+                .ForMember(dest => dest.Nome, opt => opt.MapFrom(src => src.Nome.Valor))
+                .ForMember(dest => dest.Descricao, opt => opt.MapFrom(src => src.Descricao.Valor))
+                .ForMember(dest => dest.PrecoOriginal, opt => opt.MapFrom(src => src.PrecoBase.Valor))
+                .ForMember(dest=>dest.PrecoAtual,opt=>opt.MapFrom(src=>src.ObterPrecoAtual().Valor));
+        }
+    }
+}
