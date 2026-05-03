c49e923b2b24ea174ff9995bb408940f2eba4f27
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-05-03 02:42:13 -0300

    adiciona Controller de jogo

--- src/FiapCloundGames.API/Controller/JogoController.cs
+++ src/FiapCloundGames.API/Controller/JogoController.cs
@@ -1,6 +1,5 @@
 ﻿using AutoMapper;
 using FiapCloundGames.API.Application.Dtos.Jogos;
-using FiapCloundGames.API.Application.Services;
 using FiapCloundGames.API.Application.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,36 +7,81 @@ namespace FiapCloundGames.API.Controller
 {
     [ApiController]
     [Route("api/[controller]")]
+    [Tags("Catálogo de Jogos")]
     public class JogoController : ControllerBase
     {
         private readonly ILogger<JogoController> _logger;
         private readonly IJogosService _jogoService;
         private readonly IMapper _mapper;
-        public JogoController(ILogger<JogoController> logger,IJogosService jogosService,IMapper mapper ) {
+        public JogoController(ILogger<JogoController> logger, IJogosService jogosService, IMapper mapper)
+        {
             _logger = logger;
             _jogoService = jogosService;
             _mapper = mapper;
-        } 
+        }
 
         [HttpGet("{id:guid}")]
+        [ProducesResponseType(typeof(JogoResponse), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<ActionResult<JogoResponse>> ObterJogoPorId(Guid id)
         {
             _logger.LogInformation("Obtém jogo por id: {Id}", id);
             var jogo = await _jogoService.ObtemJogoPorId(id);
             if (jogo is null) return NotFound();
 
-            var jogoResponse = _mapper.Map<JogoResponse>(jogo);
-
-            return Ok(jogoResponse);
+            return Ok(_mapper.Map<JogoResponse>(jogo));
         }
 
-        [Tags("Catálogo de jogos")]
         [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<JogoResponse>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<ActionResult<IEnumerable<JogoResponse>>> ObtemCatalogoDeJogos()
         {
             _logger.LogInformation("Recupera catálogo de jogos");
             var jogos = await _jogoService.ObtemCatalagoJogos();
-            return jogos.Any() ? Ok(jogos) : NoContent();
+            if (!jogos.Any()) return NoContent();
+            return Ok(jogos);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(JogoResponse), StatusCodes.Status201Created)]        
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<JogoResponse>> Adicionar(CriarJogoRequest jogoRequest)
+        {
+            _logger.LogInformation("Recebida requisição para adicionar o jogo: {NomeJogo}", jogoRequest.Nome);
+
+            await _jogoService.VerificaDuplicidadeNome(jogoRequest.Nome);
+
+            var jogo = await _jogoService.AdicionaJogo(jogoRequest);
+            return  CreatedAtAction(nameof(ObterJogoPorId), new { id = jogo.Id }, jogo);
+        }
+
+        [HttpDelete("{id:guid}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult> Desativar(Guid id)
+        {
+            _logger.LogInformation("Recebida requisição para deletar jogo. ID: {Id}", id);
+
+            await _jogoService.Desativar(id);
+
+            _logger.LogInformation("Jogo {Id} desativado com sucesso",id);
+            return NoContent();
+        }
+
+        [HttpPut("{id:guid}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<JogoResponse>> Atualizar(Guid id, UpdateJogoRequest updateRequest)
+        {
+            _logger.LogInformation("Recebida requisição para atualizar jogo. ID: {Id}", id);
+            var jogo = await _jogoService.ObtemJogoPorId(id);
+            if(jogo == null)
+                return NotFound();
+
+            return _mapper.Map<JogoResponse>(await _jogoService.AtualizarJogo(id,updateRequest));            
         }
     }
 }
