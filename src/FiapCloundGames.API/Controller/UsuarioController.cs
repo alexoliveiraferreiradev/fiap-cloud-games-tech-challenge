414b9e5560a42e9a351b3405ffd54b2d958daeb3
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-05-03 02:48:34 -0300

    adiciona controller de usuario e auth

---
+++ src/FiapCloundGames.API/Controller/UsuarioController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FiapCloundGames.API.Controller
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsuarioController : ControllerBase
+    {       
+    }
+}
