0b22553bc862742792e12f3e57b122fbfc588674
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-05-03 13:34:31 -0300

    adiciona LoginResponse

---
+++ src/FiapCloundGames.API/Application/Dtos/Usuario/LoginResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace FiapCloundGames.API.Application.Dtos.Usuario
+{
+    public class LoginResponse
+    {
+        public string AcessToken { get; set; }
+        public double ExpiresIn { get; set; }
+        public string Id { get; set; }
+        public string Email { get; set; }
+        public IEnumerable<ClaimResponse> Claims { get; set; }
+    }
+}
