905af477f5dd14d41c2f3ddedb3cef1f9688c550
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-05-02 23:45:27 -0300

    adiciona injeção de dependência

--- src/FiapCloundGames.API/Program.cs
+++ src/FiapCloundGames.API/Program.cs
@@ -1,4 +1,9 @@
+using FiapCloundGames.API.Application.Services;
+using FiapCloundGames.API.Application.Services.Interfaces;
+using FiapCloundGames.API.Domain.Repositories;
+using FiapCloundGames.API.Endpoints;
 using FiapCloundGames.API.Infrastructure.Persistance.Context;
+using FiapCloundGames.API.Infrastructure.Repository;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,14 +17,25 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
     options.UseSqlServer(connectionString);
 });
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<ApplicationDbContext>();
+builder.Services.AddScoped<IJogoRepository, JogoRepository>();
+builder.Services.AddScoped<IJogosService, JogosService>();
 
 var app = builder.Build();
-
+app.AddJogoEndpoint();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Fiap Cloud Games V1");
+    });
 }
 
 app.UseHttpsRedirection();
