cc26b8fc51c2a32f61b3d56c4f84603466f42ef0
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-05-04 17:22:40 -0300

    simplifica program com classes de extensão

--- src/FiapCloundGames.API/Program.cs
+++ src/FiapCloundGames.API/Program.cs
@@ -8,19 +8,7 @@ builder.AddApiConfiguration()
 
 
 var app = builder.Build();
-app.UseMiddleware<ExceptionMiddleware>();
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-    app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Fiap Cloud Games V1");
-    });
-}
+app.EnviromentConfiguration()
+    .AddAppConfiguration();
 
-app.MapControllers();
-app.UseAuthentication();
-app.UseAuthorization();
-app.UseHttpsRedirection();
 app.Run();
