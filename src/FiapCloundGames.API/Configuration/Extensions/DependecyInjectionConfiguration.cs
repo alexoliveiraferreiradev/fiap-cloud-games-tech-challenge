b5ed22eede1f25f63e92d5d0b125e03a38393146
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-05-03 21:11:30 -0300

    adiciona injenção de depedência configuration

---
+++ src/FiapCloundGames.API/Configuration/Extensions/DependecyInjectionConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using FiapCloundGames.API.Application.Services;
+using FiapCloundGames.API.Application.Services.Interfaces;
+using FiapCloundGames.API.Domain.Common.Interfaces;
+using FiapCloundGames.API.Domain.Repositories;
+using FiapCloundGames.API.Infrastructure.Persistance.Context;
+using FiapCloundGames.API.Infrastructure.Repository;
+using FiapCloundGames.API.Infrastructure.Security;
+
+namespace FiapCloundGames.API.Configuration.Extensions
+{
+    public static class DependecyInjectionConfiguration
+    {
+        public static WebApplicationBuilder AddDependecyInjectionConfiguration(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddScoped<ApplicationDbContext>();
+            builder.Services.AddScoped<IToken, TokenConfiguration>();
+            builder.Services.AddScoped<IUsuarioService, UsuarioService>();
+            builder.Services.AddScoped<IUsuarioRepository, UsuarioRepository>();
+            builder.Services.AddScoped<IPasswordHasher, PasswordHasher>();
+            builder.Services.AddScoped<IJogoRepository, JogoRepository>();
+            builder.Services.AddScoped<IJogosService, JogosService>();
+            return builder;
+        }
+    }
+}
