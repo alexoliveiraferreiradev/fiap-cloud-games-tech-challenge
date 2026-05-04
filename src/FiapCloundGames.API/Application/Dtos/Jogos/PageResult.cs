4d61ef1984504912fecccf2b3bf1d81c3b0e2093
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-05-04 16:46:42 -0300

    modifica o PagedResult para JsonSerializer

--- src/FiapCloundGames.API/Application/Dtos/Jogos/PageResult.cs
+++ src/FiapCloundGames.API/Application/Dtos/Jogos/PageResult.cs
@@ -1,19 +1,27 @@
-﻿namespace FiapCloundGames.API.Application.Dtos.Jogos
+﻿using System.Text.Json.Serialization;
+
+namespace FiapCloundGames.API.Application.Dtos.Jogos
 {
     public class PagedResult<T>
     {
-        public IEnumerable<T> Items { get; set; }
+        public IEnumerable<T> Items { get; set; } = new List<T>();
         public int PageNumber { get; set; }
         public int PageSize { get; set; }
         public int TotalItems { get; set; }
         public int TotalPages => (int)Math.Ceiling(TotalItems / (double)PageSize);
 
+        
         public PagedResult(IEnumerable<T> items, int pageNumber, int pageSize, int totalItems)
         {
             Items = items;
             PageNumber = pageNumber;
             PageSize = pageSize;
             TotalItems = totalItems;
+        }
+
+        public PagedResult()
+        {
+            
         }
     }
 }
