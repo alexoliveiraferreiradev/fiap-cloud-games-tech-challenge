a08c7bb809458df8a1f76eb873ee5488638f4a4f
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-05-04 16:49:11 -0300

    adiciona o Conxtet na pasta

---
+++ src/FiapCloundGames.API/Infrastructure/Persistance/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using FiapCloundGames.API.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace FiapCloundGames.API.Infrastructure.Persistance
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options){            
+        }
+
+        public DbSet<Usuario> Usuarios { get; set; }    
+        public DbSet<Jogo> Jogos { get; set; }  
+        public DbSet<Biblioteca> Bibliotecas { get; set; }
+        public DbSet<Pedido> Pedidos {  get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
