7a6fd421904727f073296c02a6275a805c5e972f
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-05-04 03:42:14 -0300

    adiciona os métodos DesativaPromocoesInvalidas, ObterJogosPorIds e ObtemJogosPromovidos

--- src/FiapCloundGames.API/Domain/Repositories/IJogoRepository.cs
+++ src/FiapCloundGames.API/Domain/Repositories/IJogoRepository.cs
@@ -7,8 +7,10 @@ namespace FiapCloundGames.API.Domain.Repositories
     {
         Task<Jogo?> ObtemPorNome(string nomeJogo);
         Task<IEnumerable<Jogo>> ObtemJogosAtivos();
-
+        Task<IEnumerable<Jogo>> ObtemJogosPromovidos();
         Task<Promocao?> ObterPromocaoPorId(Guid id);
         Task<IEnumerable<Jogo>> ObtemPorGenero(GeneroJogo generoJogo);
+        Task<IEnumerable<Jogo>> ObterJogosPorIds(IEnumerable<Guid> jogosIds);
+        Task DesativaPromocoesInvalidas();
     }
 }
