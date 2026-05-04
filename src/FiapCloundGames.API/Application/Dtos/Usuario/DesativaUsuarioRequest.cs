ac6d2dc984d8a9985ee993683fced09abb0b8eb0
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-05-03 21:09:25 -0300

    adiciona a classe DesativaUsuarioRequest

---
+++ src/FiapCloundGames.API/Application/Dtos/Usuario/DesativaUsuarioRequest.cs
@@ -0,0 +1,21 @@
+﻿using FiapCloundGames.API.Domain.Enum;
+
+namespace FiapCloundGames.API.Application.Dtos.Usuario
+{
+    public class DesativaUsuarioRequest
+    {
+        public Guid Id { get; set; }
+        public MotivoExclusao MotivoDelecao { get; set; }
+
+        public DesativaUsuarioRequest()
+        {            
+        }
+
+        public DesativaUsuarioRequest(Guid id, MotivoExclusao motivoDelecao)
+        {
+            Id = id;
+            MotivoDelecao = motivoDelecao;
+        }
+    }
+        
+}
