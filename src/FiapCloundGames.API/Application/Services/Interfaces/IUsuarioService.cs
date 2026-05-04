e6603a06c28e7b2864ce383442f6eae09f3378e2
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-05-03 21:10:43 -0300

    passa o DesativaUsuarioRequest como parâmetro

--- src/FiapCloundGames.API/Application/Services/Interfaces/IUsuarioService.cs
+++ src/FiapCloundGames.API/Application/Services/Interfaces/IUsuarioService.cs
@@ -10,14 +10,14 @@ namespace FiapCloundGames.API.Application.Services.Interfaces
         Task<Usuario> RebaixarParaJogador(Guid id,Guid idOperador);
         Task<Usuario> CadastrarUsuario(CriaUsuarioRequest request);
         Task<Usuario> AtualizarUsuario(Guid usuarioId, UpdateUsuarioRequest updateUsuarioRequest);
-        Task Desativar(DeleteUsuarioRequest deleteUsuarioRequest,Guid idOperador);
-        Task DesativarConta(DeleteUsuarioRequest deleteUsuarioRequest);
-
+        Task Desativar(DesativaUsuarioRequest deleteUsuarioRequest,Guid idOperador);
+        Task DesativarConta(Guid id);
         Task Reativar(Guid usuarioId);
         Task<Usuario> Autenticar(LoginRequest loginRequest);
         Task<Usuario> ObterPorId(Guid usuarioId);
         Task<IEnumerable<Usuario>> ObterTodos();
         Task<Usuario?> ObterPorEmail(string emailUsuario);
+        Task<bool> VerificaAdminCadastrado();
 
     }
 }
