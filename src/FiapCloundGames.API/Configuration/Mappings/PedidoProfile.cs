1ff78f7bfe0777126790e0328c1a90582f996c26
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-05-04 03:31:16 -0300

    adiciona mapper de Pedido

---
+++ src/FiapCloundGames.API/Configuration/Mappings/PedidoProfile.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using FiapCloundGames.API.Application.Dtos.Pedido;
+using FiapCloundGames.API.Domain.Entities;
+
+namespace FiapCloundGames.API.Configuration.Mapping
+{
+    public class PedidoProfile : Profile
+    {
+        public PedidoProfile()
+        {
+            CreateMap<Pedido, PedidoResponse>()
+            .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
+            .ForMember(dest => dest.DataPedido, opt => opt.MapFrom(src => src.DataCadastro))
+            .ForMember(dest => dest.Status, opt => opt.MapFrom(src => src.Status.ToString()))
+            .ForMember(dest => dest.Items, opt => opt.MapFrom(src => src.Jogos))
+            .ForMember(dest => dest.ValorTotal, opt => opt.MapFrom(src => src.ValorTotal.Valor));
+            CreateMap<PedidoJogo, PedidoItemResponse>()
+            .ForMember(dest => dest.NomeJogo, opt => opt.MapFrom(src => src.Jogo.Nome.Valor))
+            .ForMember(dest => dest.PrecoOriginal, opt => opt.MapFrom(src => src.Jogo.PrecoBase.Valor))
+            .ForMember(dest => dest.PrecoPago, opt => opt.MapFrom(src => src.Jogo.ObterPrecoAtual().Valor));
+
+        }
+    }
+}
