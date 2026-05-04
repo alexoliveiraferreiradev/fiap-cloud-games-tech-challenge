8349f7dfe61b64d08c3132bb990ea803a8924fc2
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-05-04 03:25:07 -0300

    adiciona dto PedidoResponse

---
+++ src/FiapCloundGames.API/Application/Dtos/Pedido/PedidoResponse.cs
@@ -0,0 +1,32 @@
+﻿using FiapCloundGames.API.Domain.Enum;
+
+namespace FiapCloundGames.API.Application.Dtos.Pedido
+{
+    public class PedidoResponse
+    {
+        public Guid Id { get; set; }
+        public Guid JogoId { get; set; }
+        public DateTime DataPedido { get; set; }
+        public PedidoStatus Status { get; set; }       
+        public decimal ValorTotal { get; set; }
+        public List<string> MensagensInformativas { get; set; } = new();
+        public IEnumerable<PedidoItemResponse> Items { get; set; }
+        public PedidoResponse()
+        {
+            
+        }
+    }
+
+    public class PedidoItemResponse
+    {
+        public string NomeJogo { get; set; }
+        public decimal PrecoOriginal { get; set; }
+        public decimal Desconto { get; set; }
+        public decimal PrecoPago { get; set; }
+
+        public PedidoItemResponse()
+        {
+            Desconto = PrecoOriginal - PrecoPago;
+        }
+    }
+}
