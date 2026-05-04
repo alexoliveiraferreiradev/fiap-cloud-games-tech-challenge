3b96fd194618e960123e3e8f4a443edaea7c7268
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-05-03 21:22:33 -0300

    adiciona Error Messages: ErroEmail e ErroNomeReal

--- src/FiapCloundGames.API/Domain/Resources/DataAnnotationMessage.cs
+++ src/FiapCloundGames.API/Domain/Resources/DataAnnotationMessage.cs
@@ -8,5 +8,7 @@
 
         public const string ErroFormato = "O campo {0} está em formato incorreto";
         public const string ErroConfirmacaoSenha = "O campo {0} é diferente da senha";
+        public const string ErroNomeReal = "Por favor, insira um nome real.";
+        public const string ErroEmail = "E-mails do domínio @example.com não são permitidos.";
     }
 }
