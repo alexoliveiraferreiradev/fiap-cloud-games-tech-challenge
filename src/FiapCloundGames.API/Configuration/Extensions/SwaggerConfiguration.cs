06aff19e26a979b16e433f29ca966fd006bf897d
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-05-03 21:11:44 -0300

    adiciona configuração do swagger

---
+++ src/FiapCloundGames.API/Configuration/Extensions/SwaggerConfiguration.cs
@@ -0,0 +1,41 @@
+﻿using FiapCloundGames.API.Filters;
+using Microsoft.OpenApi.Models;
+
+namespace FiapCloundGames.API.Configuration.Extensions
+{
+    public static class SwaggerConfiguration
+    {
+        public static WebApplicationBuilder AddSwaggerConfiguration(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo { Title = "Fiap Cloud Games", Version = "v1" });
+                options.SchemaFilter<EnumSchemaFilter>();
+
+                options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "Insira o token JWT: Bearer {Seu Token}",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+            });
+            return builder;
+        }
+    }
+}
