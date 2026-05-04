9c4c59fa7d1c646ac9ab8bad85f3f0d63901ced7
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-05-04 11:22:47 -0300

    altera retorno dos métodos para o dto

--- src/FiapCloundGames.API/Application/Services/Interfaces/IJogosService.cs
+++ src/FiapCloundGames.API/Application/Services/Interfaces/IJogosService.cs
@@ -8,19 +8,19 @@ namespace FiapCloundGames.API.Application.Services.Interfaces
 {
     public interface IJogosService
     {
-        Task<Jogo> AdicionaJogo(CriarJogoRequest request);
-        Task<Jogo> AtualizarJogo(Guid jogoId,UpdateJogoRequest updateJogoRequest);
+        Task<JogoResponse> AdicionaJogo(CriarJogoRequest request);
+        Task<JogoResponse> AtualizarJogo(Guid jogoId,UpdateJogoRequest updateJogoRequest);
         Task Desativar(Guid jogoId);
         Task Reativar(Guid jogoId);
         Task<bool> VerificaDuplicidadeNome(string nomeJogo);
         Task AdicionarPromocao(CriaPromocaoRequest criaPromocaoRequest);
         Task AtualizaPromocao(Guid promocaoId,UpdatePromocaoRequest criaPromocaoRequest);
         Task DesativarPromocao(Guid promocaoId);
-        Task<IEnumerable<Jogo>> ObtemCatalagoJogos();
-        Task<Jogo> ObtemJogoPorId(Guid jogoId);
-        Task<Promocao?> ObtemPromocaoPorId(Guid promocaoId);
-        Task<IEnumerable<Jogo>> ObtemPorGenero(GeneroJogo generoJogo);
-        Task<IEnumerable<Jogo>> ObtemJogosPromovidos();
+        Task<IEnumerable<JogoResponse>> ObtemCatalagoJogos();
+        Task<JogoResponse> ObtemJogoPorId(Guid jogoId);
+        Task<PromocaoResponse?> ObtemPromocaoPorId(Guid promocaoId);
+        Task<IEnumerable<JogoResponse>> ObtemPorGenero(GeneroJogo generoJogo);
+        Task<IEnumerable<JogoResponse>> ObtemJogosPromovidos();
         Task DesativaPromocoesInvalidas();
     }
 }
