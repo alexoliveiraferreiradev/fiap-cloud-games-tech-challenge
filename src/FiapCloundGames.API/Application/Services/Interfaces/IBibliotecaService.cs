486c173241eb5ad703dee481b290aedc945ecba9
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-05-04 03:28:20 -0300

    adiciona métodos ObterIdsJogosDoUsuario e VerificaSeUsuarioPossuiJogo

--- src/FiapCloundGames.API/Application/Services/Interfaces/IBibliotecaService.cs
+++ src/FiapCloundGames.API/Application/Services/Interfaces/IBibliotecaService.cs
@@ -1,10 +1,13 @@
 ﻿using FiapCloundGames.API.Application.Dtos.Biblioteca;
+using FiapCloundGames.API.Domain.Entities;
 
 namespace FiapCloundGames.API.Application.Services.Interfaces
 {
     public interface IBibliotecaService
     {
         Task LiberarJogosAposPedido(Guid usuarioId, List<Guid> jogosIds);
-        Task<IEnumerable<BibliotecaResponse>> ObterJogosPorUsuario(Guid usuarioId);
+        Task<IEnumerable<Biblioteca>> ObterJogosPorUsuario(Guid usuarioId);
+        Task<bool> VerificaSeUsuarioPossuiJogo(Guid usuarioId, Guid jogoId);
+        Task<IEnumerable<Guid>> ObterIdsJogosDoUsuario(Guid usuarioId);
     }
 }
