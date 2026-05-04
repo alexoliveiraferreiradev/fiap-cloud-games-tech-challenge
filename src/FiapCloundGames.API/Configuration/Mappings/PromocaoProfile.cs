d60df674baa5c847cdf06fb01ab33c331c669b64
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-05-04 03:34:13 -0300

    adiciona profile de promoção

---
+++ src/FiapCloundGames.API/Configuration/Mappings/PromocaoProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using FiapCloundGames.API.Application.Dtos.Promocao;
+using FiapCloundGames.API.Domain.Entities;
+
+namespace FiapCloundGames.API.Configuration.Mapping
+{
+    public class PromocaoProfile : Profile
+    {
+        public PromocaoProfile()
+        {
+            CreateMap<Promocao, PromocaoResponse>()
+                .ForMember(dest=>dest.JogoId,opt=>opt.MapFrom(src=>src.JogoId))
+                .ForMember(dest=>dest.ValorPromocao,opt=>opt.MapFrom(src=>src.ValorPromocao.Valor))
+                .ForMember(dest=>dest.DataFim,opt=>opt.MapFrom(src=>src.Periodo.DataFim));
+
+            CreateMap<Jogo, PromocaoResponse>()
+                .ForMember(dest => dest.NomeJogo, opt => opt.MapFrom(src => src.Nome.Valor))
+                .ForMember(dest => dest.DescricaoJogo, opt => opt.MapFrom(src => src.Descricao.Valor));
+        }
+    }
+}
