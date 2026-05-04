055202cdf396b5036bbc913cbd24cb3e5350136a
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-05-04 03:24:48 -0300

    adiciona primeira migration

--- src/FiapCloundGames.API/Infrastructure/Persistance/Migrations/20260504015100_Primeira_Migration.cs
+++ src/FiapCloundGames.API/Infrastructure/Persistance/Migrations/20260504015100_Primeira_Migration.cs
@@ -5,7 +5,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 #pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
 
-namespace FiapCloundGames.API.Infrastructure.Persistance.Migrations
+namespace FiapCloundGames.API.Infrastructure.Persistance
 {
     /// <inheritdoc />
     public partial class Primeira_Migration : Migration
@@ -171,7 +171,7 @@ namespace FiapCloundGames.API.Infrastructure.Persistance.Migrations
             migrationBuilder.InsertData(
                 table: "Usuarios",
                 columns: new[] { "Id", "Ativo", "DataAlteracao", "DataCadastro", "MotivoDesativacao", "Perfil", "Email", "Nome", "Senha" },
-                values: new object[] { new Guid("aea0b4f3-d220-4c8d-aba8-d868be7ca593"), true, new DateTime(2026, 5, 2, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2026, 5, 2, 0, 0, 0, 0, DateTimeKind.Unspecified), null, 2, "admin@fiapcloudgames.com.br", "Administrador Sistema", "$2a$11$Soy4TsNUDtuazT6CJulPleFnp82cF5BkICiOmF9sk19x0X6pMAic." });
+                values: new object[] { new Guid("aea0b4f3-d220-4c8d-aba8-d868be7ca593"), true, new DateTime(2026, 5, 2, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2026, 5, 2, 0, 0, 0, 0, DateTimeKind.Unspecified), null, 1, "admin@fiapcloudgames.com.br", "Administrador Sistema", "$2a$11$Soy4TsNUDtuazT6CJulPleFnp82cF5BkICiOmF9sk19x0X6pMAic." });
 
             migrationBuilder.CreateIndex(
                 name: "IX_Bibliotecas_JogoId",
