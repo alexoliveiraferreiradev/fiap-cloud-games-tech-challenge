6d1772da93e5c4e9a004bd3c826291ae6dba4acb
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-05-05 00:13:50 -0300

    mapeia bool de Ativo no JogoResponse

--- src/FiapCloundGames.API/Configuration/Mappings/JogoProfile.cs
+++ src/FiapCloundGames.API/Configuration/Mappings/JogoProfile.cs
@@ -13,6 +13,7 @@ namespace FiapCloundGames.API.Configuration.Mapping
                 .ForMember(dest => dest.Nome, opt => opt.MapFrom(src => src.Nome.Valor))
                 .ForMember(dest => dest.Descricao, opt => opt.MapFrom(src => src.Descricao.Valor))
                 .ForMember(dest => dest.PrecoOriginal, opt => opt.MapFrom(src => src.PrecoBase.Valor))
+                .ForMember(dest=>dest.Ativo,opt=>opt.MapFrom(src=>src.Ativo))
                 .ForMember(dest=>dest.PrecoAtual,opt=>opt.MapFrom(src=>src.ObterPrecoAtual().Valor));
 
             CreateMap<Jogo,PromocaoResponse>()
