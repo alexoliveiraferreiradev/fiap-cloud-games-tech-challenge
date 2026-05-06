8b67bd4bf94a4a503eb3d7258441bbb5911dc924
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-05-05 21:38:27 -0300

    adiciona JogoUsuarioReponse, evitando mostrar informações de JogoAtivo e PrecoBase para o usuário

---
+++ src/FiapCloundGames.API/Application/Dtos/Jogos/JogoUsuarioResponse.cs
@@ -0,0 +1,14 @@
+﻿using FiapCloundGames.API.Domain.Enum;
+
+namespace FiapCloundGames.API.Application.Dtos.Jogos
+{
+    public class JogoUsuarioResponse
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public decimal PrecoAtual { get; set; }
+        public GeneroJogo Genero { get; set; }
+        public bool TemDesconto{ get; set; }
+    }
+}
