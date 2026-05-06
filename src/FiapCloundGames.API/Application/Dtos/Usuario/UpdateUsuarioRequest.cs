0d1313071a881bb006b6302352a348acc9a59179
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-05-05 21:55:29 -0300

    retira Email do usuario do dto, afim de não atualizar email no sistema

--- src/FiapCloundGames.API/Application/Dtos/Usuario/UpdateUsuarioRequest.cs
+++ src/FiapCloundGames.API/Application/Dtos/Usuario/UpdateUsuarioRequest.cs
@@ -5,19 +5,26 @@ namespace FiapCloundGames.API.Application.Dtos.Usuario
 {
     public class UpdateUsuarioRequest
     {
+        /// <summary>
+        /// Nome do usuário
+        /// </summary>
+        /// <example>Novo nome do usuário</example>
         [Required(ErrorMessage = DataAnnotationMessage.ErroRequired)]
         [RegularExpression(@"^(?i)(?!string$).*", ErrorMessage = DataAnnotationMessage.ErroNomeReal)]
         [StringLength(40, ErrorMessage = DataAnnotationMessage.ErroCaracteres, MinimumLength = 3)]
-        public string NomeUsuario { get; set; }
-        [Required(ErrorMessage = DataAnnotationMessage.ErroRequired)]
-        [EmailAddress(ErrorMessage = DataAnnotationMessage.ErroFormato)]
-        [RegularExpression(@"^(?!.*@example\.com$).*", ErrorMessage = DataAnnotationMessage.ErroEmail)]
-        [StringLength(100, ErrorMessage = DataAnnotationMessage.ErroCaracteres, MinimumLength = 7)]
-        public string EmailUsuario { get; set; }
+        public string NomeUsuario { get; set; }      
+        /// <summary>
+        /// Senha do usuário
+        /// </summary>
+        /// <example>Nova senha do usuário</example>
         [Required(ErrorMessage = DataAnnotationMessage.ErroRequired)]
         [DataType(DataType.Password, ErrorMessage = DataAnnotationMessage.ErroFormato)]
         [StringLength(60, ErrorMessage = DataAnnotationMessage.ErroCaracteres, MinimumLength = 8)]
         public string SenhaUsuario { get; set; }
+        /// <summary>
+        /// Confirmação de senha
+        /// </summary>
+        /// <example>Confirmação de senha do usuário</example>
         [Required(ErrorMessage = DataAnnotationMessage.ErroRequired)]
         [DataType(DataType.Password)]
         [Compare("SenhaUsuario", ErrorMessage = DataAnnotationMessage.ErroConfirmacaoSenha)]
@@ -28,7 +35,7 @@ namespace FiapCloundGames.API.Application.Dtos.Usuario
         }
         public UpdateUsuarioRequest(string novoNome, string novoEmail, string novaSenha, string novaConfirmaoSenha)
         {
-            NomeUsuario = novoNome; EmailUsuario = novoEmail; SenhaUsuario = novaSenha; ConfirmacaoSenha = novaConfirmaoSenha;
+            NomeUsuario = novoNome; SenhaUsuario = novaSenha; ConfirmacaoSenha = novaConfirmaoSenha;
         }
     }
 }
