d112ed5c2e02e3f2fdf19a64956d9a379c17f99c
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-05-08 16:57:12 -0300

    feat: add JogoFiltroRequest, visando ter os filtros para paginação de jogo

---
+++ src/FiapCloudGames.Application/Dtos/Jogos/JogoFiltroRequest.cs
@@ -0,0 +1,13 @@
+﻿using FiapCloudGames.Domain.Enum;
+
+namespace FiapCloudGames.Application.Dtos.Jogos
+{
+    public class JogoFiltroRequest
+    {
+        public int Pagina { get; set; } = 1;
+        public int Tamanho { get; set; } = 10;
+        public string? Busca { get; set; }
+        public GeneroJogo? Genero { get; set; }
+        public bool? ApenasPromovidos { get; set; } 
+    }
+}
