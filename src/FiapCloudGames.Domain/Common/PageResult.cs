ef8c22e86f1826c71f1a36f16136b0cccce45802
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-05-08 16:49:50 -0300

    feat: add PageResult para paginação de listas

---
+++ src/FiapCloudGames.Domain/Common/PageResult.cs
@@ -0,0 +1,26 @@
+﻿
+namespace FiapCloudGames.Domain.Common
+{
+    public class PagedResult<T>
+    {
+        public IEnumerable<T> Itens { get; set; } = new List<T>();
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+        public int TotalItens { get; set; }
+        public int TotalPages => (int)Math.Ceiling(TotalItens / (double)PageSize);
+
+        
+        public PagedResult(IEnumerable<T> itens, int pageNumber, int pageSize, int totalItems)
+        {
+            Itens = itens;
+            PageNumber = pageNumber;
+            PageSize = pageSize;
+            TotalItens = totalItems;
+        }
+
+        public PagedResult()
+        {
+            
+        }
+    }
+}
