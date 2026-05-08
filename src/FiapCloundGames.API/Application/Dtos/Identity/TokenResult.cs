ab5c0c415bee9cfa831fa564a4f0de3a0ff06ff5
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-05-08 00:43:06 -0300

    feat: add TokenResult para obter dados do token gerado

---
+++ src/FiapCloundGames.API/Application/Dtos/Identity/TokenResult.cs
@@ -0,0 +1,11 @@
+﻿using FiapCloundGames.API.Application.Dtos.Usuario;
+
+namespace FiapCloundGames.API.Application.Dtos.Identity
+{
+    public class TokenResult
+    {
+        public string AccessToken { get; set; }
+        public double ExpiresIn { get; set; }
+        public IEnumerable<ClaimResponse> Claims { get; set; }
+    }
+}
