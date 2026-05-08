b07cdf463411e9b45b552fb338da78beabc787e4
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-05-08 00:54:52 -0300

    feat: add ITokenService para geração do Jwt do usuário

---
+++ src/FiapCloundGames.API/Application/Services/Interfaces/ITokenService.cs
@@ -0,0 +1,13 @@
+﻿using FiapCloundGames.API.Application.Dtos.Identity;
+using FiapCloundGames.API.Application.Dtos.Usuario;
+using System.Security.Claims;
+
+namespace FiapCloundGames.API.Application.Services.Interfaces
+{
+    public interface ITokenService
+    {
+        string ObtemToken(IEnumerable<Claim> claims);
+        Task<IEnumerable<Claim>> ObtemClaims(UsuarioResponse usuario);
+        Task<TokenResult> GerarToken(UsuarioResponse usuario);
+    }
+}
