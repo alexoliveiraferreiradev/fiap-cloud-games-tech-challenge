918a35adf80ec6c06676080400e7d5ee046e5e7e
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-05-08 16:43:12 -0300

    feat: add RedisExtensions para cofiguração do redis no projeto

---
+++ src/FiapCloundGames.API/Extensions/RedisExtensions.cs
@@ -0,0 +1,26 @@
+﻿using FiapCloudGames.Infrastructure.Caching;
+using StackExchange.Redis;
+
+namespace FiapCloudGames.API.Extensions
+{
+    public static class RedisExtensions
+    {
+        public static WebApplicationBuilder AddRedis(this WebApplicationBuilder builder)
+        {
+            var redisConfig = builder.Configuration.GetSection("Redis").Get<RedisOptions>();
+            builder.Services.AddSingleton<IConnectionMultiplexer>(sp =>
+            {
+                var configuration = ConfigurationOptions.Parse(redisConfig.Configuration, true);
+                return ConnectionMultiplexer.Connect(configuration);
+            });
+
+            builder.Services.AddStackExchangeRedisCache(options =>
+            {
+                options.Configuration = redisConfig.Configuration;
+                options.InstanceName = redisConfig.InstanceName;
+            });
+
+            return builder;
+        }
+    }
+}
