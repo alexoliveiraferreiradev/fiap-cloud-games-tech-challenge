2b5cebac7976d84dc84ef62e29786283c78d27a8
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-05-08 16:48:41 -0300

    refactor: altera nome das referências dos namespaces

--- src/FiapCloundGames.API/Controller/JogoController.cs
+++ src/FiapCloundGames.API/Controller/JogoController.cs
@@ -1,5 +1,5 @@
 ﻿using FiapCloudGames.Application.Dtos.Jogos;
-using FiapCloudGames.Application.Services.Interfaces;
+using FiapCloudGames.Application.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
