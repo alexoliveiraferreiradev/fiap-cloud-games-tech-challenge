acee7dea3c7995679323adb020b8e73c10cfbb3a
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-05-08 00:52:20 -0300

    refactor: muda retorno Autenicar e Cadastrar para LoginResponse

--- src/FiapCloundGames.API/Application/Services/Interfaces/IUsuarioService.cs
+++ src/FiapCloundGames.API/Application/Services/Interfaces/IUsuarioService.cs
@@ -1,4 +1,5 @@
-﻿using FiapCloundGames.API.Application.Dtos.Usuario;
+﻿using FiapCloundGames.API.Application.Dtos.Identity;
+using FiapCloundGames.API.Application.Dtos.Usuario;
 using FiapCloundGames.API.Domain.Entities;
 using FiapCloundGames.API.Domain.Enum;
 
@@ -8,12 +9,12 @@ namespace FiapCloundGames.API.Application.Services.Interfaces
     {
         Task<UsuarioResponse> PromoverParaAdmin(Guid id);
         Task<UsuarioResponse> RebaixarParaJogador(Guid id,Guid idOperador);
-        Task<UsuarioResponse> CadastrarUsuario(CriaUsuarioRequest request);
-        Task<UsuarioResponse> AtualizarUsuario(Guid usuarioId, UpdateUsuarioRequest updateUsuarioRequest);
+        Task<LoginResponse> Cadastrar(CriaUsuarioRequest request);
+        Task<UsuarioResponse> Atualizar(Guid usuarioId, UpdateUsuarioRequest updateUsuarioRequest);
         Task Desativar(DesativaUsuarioRequest deleteUsuarioRequest,Guid idOperador);
         Task DesativarConta(Guid id);
         Task Reativar(Guid usuarioId);
-        Task<UsuarioResponse> Autenticar(LoginRequest loginRequest);
+        Task<LoginResponse> Autenticar(LoginRequest loginRequest);
         Task<UsuarioResponse> ObterPorId(Guid usuarioId);
         Task<IEnumerable<UsuarioResponse>> ObterTodos();
         Task<UsuarioResponse?> ObterPorEmail(string emailUsuario);
