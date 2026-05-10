0ac11ab33570c0b2d31321fc92a5c2b0a2d8e905
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-05-10 00:31:10 -0300

    refactor: remove o obter total jogos por usuario e adiciona o obter biblioteca paginado pelo PagedResult

--- src/FiapCloudGames.Domain/Repositories/IBibliotecaRepository.cs
+++ src/FiapCloudGames.Domain/Repositories/IBibliotecaRepository.cs
@@ -1,12 +1,12 @@
-﻿using FiapCloudGames.Domain.Entities;
+﻿using FiapCloudGames.Domain.Common;
+using FiapCloudGames.Domain.Entities;
 
 namespace FiapCloudGames.Domain.Repositories
 {
     public interface IBibliotecaRepository : IRepository<Biblioteca>
     {
         Task<bool> VerificaSeUsuarioPossuiJogo(Guid usuarioId, Guid jogoId);
-        Task<IEnumerable<Biblioteca>> ObterJogosPorUsuarioPaginacao(Guid usuarioId, int pagina =1, int tamanhoPagina = 10);
-        Task<int> TotalJogosPorUsuario(Guid usuarioId);
+        Task<PagedResult<Biblioteca>> ObterJogosPorUsuarioPaginacao(Guid usuarioId, int pagina =1, int tamanhoPagina = 10);        
         Task<IEnumerable<Guid>> ObterIdsJogosDoUsuario(Guid usuarioId);
     }
 }
